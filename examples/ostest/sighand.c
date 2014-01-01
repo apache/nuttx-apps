7ca7ae9fd63ed2de161e433fd060bd2b3a86e597
Gregory Nutt <gnutt@nuttx.org>
2014-01-01 12:40:18 -0600

    Add Z16F NSH configuration

--- examples/ostest/sighand.c
+++ examples/ostest/sighand.c
@@ -59,7 +59,7 @@ static void death_of_child(int signo, siginfo_t *info, void *ucontext)
 {
   /* Use of printf in a signal handler is NOT safe! It can cause deadlocks!
    * Also, signals are not queued by NuttX.  As a consequence, some
-   * notifications will get lost (or the info data can be overwrittedn)! 
+   * notifications will get lost (or the info data can be overwrittedn)!
    * Because POSIX  does not require signals to be queued, I do not think
    * that this is a bug (the overwriting is a bug, however).
    */
