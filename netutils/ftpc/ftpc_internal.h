7fca1ae30c3179b3d5fcdb290ae122ea955648e3
Gregory Nutt <gnutt@nuttx.org>
2014-08-21 11:16:55 -0600

    wdog.h does not contain any application interface, only internal OS interface.  Further, it is non-standard.  Move wdog.h from include/ to include/nuttx. For the same reason, move the description of the watchdog timer interfaces from the Users Guide to the Porting Guide.

--- netutils/ftpc/ftpc_internal.h
+++ netutils/ftpc/ftpc_internal.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  * apps/netutils/ftpc/ftpc_internal.h
  *
- *   Copyright (C) 2011 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2011, 2014 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -49,7 +49,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <time.h>
-#include <wdog.h>
+
+#include <nuttx/wdog.h>
 
 #include <netinet/in.h>
 
