f7a7c696d0b38e02451c0d0ab6a7877a62d01b82
Gregory Nutt <gnutt@nuttx.org>
2014-11-04 15:51:19 -0600

    BAS:  ALL files are not close to the NuttX coding style.. except in variable and function naming

--- interpreters/bas/token.h
+++ interpreters/bas/token.h
@@ -1,11 +1,94 @@
-#ifndef TOKEN_H
-#define TOKEN_H
+/****************************************************************************
+ * apps/interpreters/bas/token.h
+ *
+ *   Copyright (c) 1999-2014 Michael Haardt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Adapted to NuttX and re-released under a 3-clause BSD license:
+ *
+ *   Copyright (C) 2014 Gregory Nutt. All rights reserved.
+ *   Authors: Alan Carvalho de Assis <Alan Carvalho de Assis>
+ *            Gregory Nutt <gnutt@nuttx.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __APPS_EXAMPLES_BAS_TOKEN_H
+#define __APPS_EXAMPLES_BAS_TOKEN_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
 
 #include "autotypes.h"
 #include "value.h"
 #include "var.h"
 
-enum SymbolType { GLOBALVAR, GLOBALARRAY, LOCALVAR, BUILTINFUNCTION, USERFUNCTION };
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define TOKEN_ISBINARYOPERATOR(t)   (Token_property[t]&1)
+#define TOKEN_ISUNARYOPERATOR(t)    (Token_property[t]&(1<<1))
+#define TOKEN_BINARYPRIORITY(t)     ((Token_property[t]>>2)&7)
+#define TOKEN_UNARYPRIORITY(t)      ((Token_property[t]>>5)&7)
+#define TOKEN_ISRIGHTASSOCIATIVE(t) (Token_property[t]&(1<<8))
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+enum SymbolType
+{
+  GLOBALVAR,
+  GLOBALARRAY,
+  LOCALVAR,
+  BUILTINFUNCTION,
+  USERFUNCTION
+};
 
 struct Symbol
 {
@@ -82,7 +165,7 @@ struct Casevalue
 
 enum TokenType
 {
-  T_NOTOKEN=0,
+  T_NOTOKEN = 0,
   T_ACCESS_READ,
   T_ACCESS_READ_WRITE,
   T_ACCESS_WRITE,
@@ -443,16 +526,21 @@ struct Token
   } u;
 };
 
-extern struct Token *Token_newCode(const char *ln);
-extern struct Token *Token_newData(const char *ln);
-extern void Token_destroy(struct Token *token);
-extern struct String *Token_toString(struct Token *token, struct Token *spaceto, struct String *s, int *indent, int full);
-extern int Token_property[];
-#define TOKEN_ISBINARYOPERATOR(t)   (Token_property[t]&1)
-#define TOKEN_ISUNARYOPERATOR(t)    (Token_property[t]&(1<<1))
-#define TOKEN_BINARYPRIORITY(t)     ((Token_property[t]>>2)&7)
-#define TOKEN_UNARYPRIORITY(t)      ((Token_property[t]>>5)&7)
-#define TOKEN_ISRIGHTASSOCIATIVE(t) (Token_property[t]&(1<<8))
-extern void Token_init(int backslash_colon, int uppercase);
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
 
-#endif
+extern int Token_property[];
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+struct Token *Token_newCode(const char *ln);
+struct Token *Token_newData(const char *ln);
+void Token_destroy(struct Token *token);
+struct String *Token_toString(struct Token *token, struct Token *spaceto,
+                              struct String *s, int *indent, int full);
+void Token_init(int backslash_colon, int uppercase);
+
+#endif /* __APPS_EXAMPLES_BAS_TOKEN_H */
