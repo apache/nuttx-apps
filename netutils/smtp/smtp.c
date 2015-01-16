7720c35502a6ff84b922bd244acb1fd8f9532113
Gregory Nutt <gnutt@nuttx.org>
2015-01-16 13:01:08 -0600

    Networking:  Replace all references to the macros net_ipaddr_copy, net_ipaddr_hdrcopy, net_ipaddr_cmp, net_ipaddr_hdrcmp, and net_ipaddr_maskcmp with the appropriate IPv4 or IPv6 version of the macro (such as net_ipv4addr_copy).  The goal is to support both IPv4 and IPv6 simultaneously.  This requires that the macros be distinct and not conditionally defined to one on or the other.

--- netutils/smtp/smtp.c
+++ netutils/smtp/smtp.c
@@ -290,7 +290,7 @@ void smtp_configure(FAR void *handle, FAR const char *lhostname,
 {
   FAR struct smtp_state *psmtp = (FAR struct smtp_state *)handle;
   psmtp->localhostname = lhostname;
-  net_ipaddr_copy(psmtp->smtpserver, paddr);
+  net_ipv4addr_copy(psmtp->smtpserver, paddr);
 }
 
 /* Send an e-mail.
