a79f3840a8d69ada28923b27e84e61552dc665c2
Gregory Nutt <gnutt@nuttx.org>
2015-05-06 07:54:22 -0600

    apps/netutils/ntp: Fix mismatched sched_lock/unlock.  Noted by Juha Niskanen

--- netutils/ntpclient/ntpclient.c
+++ netutils/ntpclient/ntpclient.c
@@ -80,7 +80,7 @@ enum ntpc_daemon_e
   NTP_STOPPED
 };
 
-/* This type describes the state of the NTP client daemon.  Only once
+/* This type describes the state of the NTP client daemon.  Only one
  * instance of the NTP daemon is permitted in this implementation.
  */
 
@@ -505,6 +505,7 @@ int ntpc_start(void)
 
           g_ntpc_daemon.state = NTP_STOPPED;
           ndbg("ERROR: Failed to start the NTP daemon\n", errval);
+          sched_unlock();
           return -errval;
         }
 
