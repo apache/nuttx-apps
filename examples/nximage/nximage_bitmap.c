322561dcf32be2d9b36ce58184647bbf33fbeb12
Gregory Nutt <gnutt@nuttx.org>
2015-07-01 14:31:28 -0600

    Fix types in #error message

--- examples/nximage/nximage_bitmap.c
+++ examples/nximage/nximage_bitmap.c
@@ -76,7 +76,7 @@
 #  endif
 
 #else
-#  error "Unsupport pixel format"
+#  error "Unsupported pixel format"
 #endif
 
 /********************************************************************************************
@@ -180,7 +180,7 @@ static const nxgl_mxpixel_t g_lut[IMAGE_NLUTCODES] =
 
 #  endif
 #else
-# error "Unsupport pixel format"
+# error "Unsupported pixel format"
 #endif
 
 #if CONFIG_EXAMPLES_NXIMAGE_BPP == 24
@@ -3458,7 +3458,7 @@ static const struct pix_run_s g_nuttx[] =
 
 #  endif
 #else
-# error "Unsupport pixel format"
+# error "Unsupported pixel format"
 #endif
 
 /********************************************************************************************
@@ -3635,7 +3635,7 @@ nxgl_mxpixel_t nximage_avgcolor(nxgl_mxpixel_t color1, nxgl_mxpixel_t color2)
   color1 = r1 << 5 | g1 << 2 | b1;
 
 #else
-# error "Unsupport pixel format"
+# error "Unsupported pixel format"
 #endif
 
   return color1;
