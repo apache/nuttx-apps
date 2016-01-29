694d6b40107ac05acab802677c48c68e61b0e267
Gregory Nutt <gnutt@nuttx.org>
2016-01-29 11:10:06 -0600

    Missed one file in last set of changes

--- platform/pcduino-a10/a1x_cxxinitialize.c
+++ platform/pcduino-a10/a1x_cxxinitialize.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * apps/platform/pcduino-a10/src/a1x_cxxinitialize.c
  *
- *   Copyright (C) 2016 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2013, 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
