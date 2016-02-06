c8e21ddcd6e985fadb62d24a44c99b64bf9d7d69
Gregory Nutt <gnutt@nuttx.org>
2016-02-06 11:34:39 -0600

    Costmetic changes to comments and spacing.

--- system/i2c/i2c_main.c
+++ system/i2c/i2c_main.c
@@ -282,6 +282,7 @@ int i2c_parse(FAR struct i2ctool_s *i2ctool, int argc, char *argv[])
           break;
         }
     }
+
   newargs[nargs] = NULL;
 
   /* Then execute the command */
@@ -351,12 +352,14 @@ int i2c_main(int argc, char *argv[])
 {
   /* Verify settings */
 
-  if (g_i2ctool.bus < CONFIG_I2CTOOL_MINBUS || g_i2ctool.bus > CONFIG_I2CTOOL_MAXBUS)
+  if (g_i2ctool.bus < CONFIG_I2CTOOL_MINBUS ||
+      g_i2ctool.bus > CONFIG_I2CTOOL_MAXBUS)
     {
       g_i2ctool.bus = CONFIG_I2CTOOL_MINBUS;
     }
 
-  if (g_i2ctool.addr < CONFIG_I2CTOOL_MINADDR || g_i2ctool.addr > CONFIG_I2CTOOL_MAXADDR)
+  if (g_i2ctool.addr < CONFIG_I2CTOOL_MINADDR ||
+      g_i2ctool.addr > CONFIG_I2CTOOL_MAXADDR)
     {
       g_i2ctool.addr = CONFIG_I2CTOOL_MINADDR;
     }
@@ -376,7 +379,7 @@ int i2c_main(int argc, char *argv[])
       g_i2ctool.freq = CONFIG_I2CTOOL_DEFFREQ;
     }
 
-  /* Parse process the command line */
+  /* Parse and process the command line */
 
   i2c_setup(&g_i2ctool);
   (void)i2c_parse(&g_i2ctool, argc, argv);
@@ -425,6 +428,7 @@ ssize_t i2ctool_write(FAR struct i2ctool_s *i2ctool, FAR const void *buffer, siz
     {
       dbg("[%d] Failed to send buffer: %d\n", OUTFD(i2ctool), errno);
     }
+
   return ret;
 }
 
