d2aa24ad88f32add1d944c4885ec11c660841569
Sebastien Lorquet <sebastien@lorquet.fr>
2016-06-09 10:51:04 -0600

    Make sure that APPNAME is defined in all Makefiles that generate applications

--- examples/mtdpart/mtdpart_main.c
+++ examples/mtdpart/mtdpart_main.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * examples/mtdpart/mtdpart_main.c
  *
- *   Copyright (C) 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2013, 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -126,10 +126,6 @@ static uint8_t g_simflash[MTDPART_BUFSIZE];
 #ifdef CONFIG_EXAMPLES_MTDPART_ARCHINIT
 extern FAR struct mtd_dev_s *mtdpart_archinitialize(void);
 #endif
-
-/****************************************************************************
- * Private Functions
- ****************************************************************************/
 
 /****************************************************************************
  * Public Functions
