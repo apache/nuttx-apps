00423662551b6ba68a5d65756967ee46d6e9d72a
Gregory Nutt <gnutt@nuttx.org>
2016-07-20 13:39:12 -0600

    can.h moved from include/nuttx/ to include/nuttx/drivers.

--- examples/canard/canard_main.c
+++ examples/canard/canard_main.c
@@ -37,7 +37,7 @@
  * Included Files
  ****************************************************************************/
 
-#include <nuttx/can.h>
+#include <nuttx/drivers/can.h>
 #include <canard.h>
 #include <canard_nuttx.h>       /* CAN backend driver for nuttx, distributed
                                  * with Libcanard */
