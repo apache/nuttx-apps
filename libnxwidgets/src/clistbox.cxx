334122ac8db9418f42635967c7689459625409e9
Petteri Aimonen <petteri.aimonen@espotel.com>
2016-09-22 11:52:52 -0600

    libnxwidgets: Avoid unnecessary redraw in NxWidgets::CListBox. The ListData will raise an event that will cause a redraw anyway.  Redrawing in click() caused double redraws.

--- libnxwidgets/src/clistbox.cxx
+++ libnxwidgets/src/clistbox.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
  * NxWidgets/libnxwidgets/src/clistbox.cxx
  *
- *   Copyright (C) 2012 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2012, 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -628,7 +628,6 @@ void CListBox::onClick(nxgl_coord_t x, nxgl_coord_t y)
     }
 
   startDragging(x, y);
-  redraw();
 }
 
 /**
