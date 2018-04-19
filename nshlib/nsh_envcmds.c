apache/nuttx-apps

commit 8ecf618a4d964731136a94517183b4972833e599

apps/nshlib: Add support for '-n' option to the echo command.

nshlib/nsh_envcmds.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  * apps/nshlib/nsh_envcmds.c
  *
- *   Copyright (C) 2007-2009, 2011-2012 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009, 2011-2012, 2018 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,18 +51,6 @@
 #include "nsh_console.h"
 
 /****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-/****************************************************************************
- * Private Types
- ****************************************************************************/
-
-/****************************************************************************
- * Private Function Prototypes
- ****************************************************************************/
-
-/****************************************************************************
  * Private Data
  ****************************************************************************/
 
@@ -71,10 +59,6 @@ static const char g_pwd[]    = "PWD";
 static const char g_oldpwd[] = "OLDPWD";
 static const char g_home[]   = CONFIG_LIB_HOMEDIR;
 #endif
-
-/****************************************************************************
- * Public Data
- ****************************************************************************/
 
 /****************************************************************************
  * Private Functions
@@ -278,17 +262,32 @@ int cmd_cd(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
 int cmd_echo(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
 {
   int i;
+  int s = 1;
+
+  if (0 == strncmp(argv[1], "-n", 2))
+    {
+      s = 2;
+    }
 
   /* echo each argument, separated by a space as it must have been on the
    * command line
    */
 
-  for (i = 1; i < argc; i++)
+  for (i = s; i < argc; i++)
     {
-      nsh_output(vtbl, "%s ", argv[i]);
+      if (i != s)
+        {
+          nsh_output(vtbl, " ");
+        }
+
+      nsh_output(vtbl, "%s", argv[i]);
     }
 
-  nsh_output(vtbl, "\n");
+  if (1 == s)
+    {
+      nsh_output(vtbl, "\n");
+    }
+
   return OK;
 }
 #endif

nshlib/nsh_command.c
@@ -186,9 +186,9 @@ static const struct cmdmap_s g_cmdmap[] =
 
 #ifndef CONFIG_NSH_DISABLE_ECHO
 # ifndef CONFIG_DISABLE_ENVIRON
-  { "echo",     cmd_echo,     1, CONFIG_NSH_MAXARGUMENTS, "[<string|$name> [<string|$name>...]]" },
+  { "echo",     cmd_echo,     1, CONFIG_NSH_MAXARGUMENTS, "[-n] [<string|$name> [<string|$name>...]]" },
 # else
-  { "echo",     cmd_echo,     1, CONFIG_NSH_MAXARGUMENTS, "[<string> [<string>...]]" },
+  { "echo",     cmd_echo,     1, CONFIG_NSH_MAXARGUMENTS, "[-n] [<string> [<string>...]]" },
 # endif
 #endif
 

commit 052a7009b3a508c8d07e4f341a3a4f68ed818d31

Merged in juniskane/apps_upstream/echo_fix_pr (pull request #135)
apps/nshlib: Fix echo command with zero arguments, broken after commit 8ecf618

Approved-by: Gregory Nutt <gnutt@nuttx.org>

nshlib/nsh_envcmds.c
@@ -264,13 +264,13 @@ int cmd_echo(FAR struct nsh_vtbl_s *vtbl, int argc, char **argv)
   int i;
   int s = 1;
 
-  if (0 == strncmp(argv[1], "-n", 2))
+  if (argc > 1 && 0 == strncmp(argv[1], "-n", 2))
     {
       s = 2;
     }
 
   /* echo each argument, separated by a space as it must have been on the
-   * command line
+   * command line.
    */
 
   for (i = s; i < argc; i++)