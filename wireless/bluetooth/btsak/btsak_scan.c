938a9b9125f1c552b9c6da616423cd37307c851f
Gregory Nutt <gnutt@nuttx.org>
2018-04-21 12:18:17 -0600

    apps/wireless/bluetooth/btsak:  Add a README file.

--- wireless/bluetooth/btsak/btsak_scan.c
+++ wireless/bluetooth/btsak/btsak_scan.c
@@ -75,7 +75,7 @@ static void btsak_scan_showusage(FAR const char *progname,
   fprintf(stderr, "\nWhere the options do the following:\n\n");
   fprintf(stderr, "\tstart\t- Starts scanning.  The -d option enables duplicate\n");
   fprintf(stderr, "\t\t  filtering.\n");
-  fprintf(stderr, "\tget\t - Shows new accumulated scan results\n");
+  fprintf(stderr, "\tget\t- Shows new accumulated scan results\n");
   fprintf(stderr, "\tstop\t- Stops scanning\n");
   exit(exitcode);
 }
