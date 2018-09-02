apache/nuttx-apps

commit 9a42be5e9b00c0594fbf5590ded17c4407670e6e

apps/nshlib:  When using waitpid() to wait for a built-in application or a file application to exit.  Add the WUNTRACED options to so the the waitpid() call will also be awakened if the waited-for task is stopped as well.

nshlib/nsh_builtin.c
@@ -153,9 +153,14 @@ int nsh_builtin(FAR struct nsh_vtbl_s *vtbl, FAR const char *cmd,
            * even if task is still active:  If the I/O was re-directed by a
            * proxy task, then the ask is a child of the proxy, and not this
            * task.  waitpid() fails with ECHILD in either case.
+           *
+           * NOTE: WUNTRACED does nothing in the default case, but in the
+           * case the where CONFIG_SIG_SIGSTOP_ACTION=y, the built-in app
+           * may also be stopped.  In that case WUNTRACED will force
+           * waitpid() to return with ECHILD.
            */
 
-          ret = waitpid(ret, &rc, 0);
+          ret = waitpid(pid, &rc, WUNTRACED);
           if (ret < 0)
             {
               /* If the child thread does not exist, waitpid() will return

nshlib/nsh_fileapps.c
@@ -185,9 +185,14 @@ int nsh_fileapp(FAR struct nsh_vtbl_s *vtbl, FAR const char *cmd,
            * Here the scheduler will be unlocked while waitpid is waiting
            * and if the application has not yet run, it will now be able to
            * do so.
+           *
+           * NOTE: WUNTRACED does nothing in the default case, but in the
+           * case the where CONFIG_SIG_SIGSTOP_ACTION=y, the file app
+           * may also be stopped.  In that case WUNTRACED will force
+           * waitpid() to return with ECHILD.
            */
 
-          ret = waitpid(pid, &rc, 0);
+          ret = waitpid(pid, &rc, WUNTRACED);
           if (ret < 0)
             {
               /* If the child thread does not exist, waitpid() will return

commit 3e396d8e4ed80933dbbe630ff3999887bbd4e639

apps/nshlib:  Fix a copy paste error introduced in commit 9a42be5e9b00c0594fbf5590ded17c4407670e6e

nshlib/nsh_builtin.c
@@ -160,7 +160,7 @@ int nsh_builtin(FAR struct nsh_vtbl_s *vtbl, FAR const char *cmd,
            * waitpid() to return with ECHILD.
            */
 
-          ret = waitpid(pid, &rc, WUNTRACED);
+          ret = waitpid(ret, &rc, WUNTRACED);
           if (ret < 0)
             {
               /* If the child thread does not exist, waitpid() will return