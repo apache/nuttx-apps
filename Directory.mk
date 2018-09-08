c8f473c5072a2962f9947e9915095c1baa492c80
Gregory Nutt <gnutt@nuttx.org>
2018-09-08 09:42:48 -0600

    Directory.mk:  Fix:  distclean targets of directory makefiles was not removing generated Kconfig files.  There is then no way to remove this Kconfig files without manually deleting them one at a time.

--- Directory.mk
+++ Directory.mk
@@ -1,7 +1,7 @@
 ############################################################################
 # apps/Directory.mk
 #
-#   Copyright (C) 2011-2015 Gregory Nutt. All rights reserved.
+#   Copyright (C) 2011-2015, 2018 Gregory Nutt. All rights reserved.
 #   Author: Gregory Nutt <gnutt@nuttx.org>
 #
 # Redistribution and use in source and binary forms, with or without
@@ -33,7 +33,7 @@
 #
 ############################################################################
 
--include $(TOPDIR)/.config # Current configuration
+-include $(TOPDIR)/Make.defs
 include $(APPDIR)/Make.defs
 
 # Sub-directories
@@ -69,5 +69,8 @@ depend: $(foreach SDIR, $(SUBDIRS), $(SDIR)_depend)
 clean: $(foreach SDIR, $(SUBDIRS), $(SDIR)_clean)
 
 distclean: $(foreach SDIR, $(SUBDIRS), $(SDIR)_distclean)
+ifneq ($(MENUDESC),)
+	$(call DELFILE, Kconfig)
+endif
 
 -include Make.dep
