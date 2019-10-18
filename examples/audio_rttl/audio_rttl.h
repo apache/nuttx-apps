46612fe1a9ec9d1c0bbf43893d881d5d69f6f08f
Gregory Nutt <gnutt@nuttx.org>
2019-10-18 08:59:06 -0600

    Trivial updates from review of PR.

--- examples/audio_rttl/audio_rttl.h
+++ examples/audio_rttl/audio_rttl.h
@@ -33,4 +33,6 @@
  *
  ****************************************************************************/
 
-FAR char *g_song = (char *)"Test:d=4,o=6,b=90:c,c#,d,d#,e,f,f#,g,a,a#,b,p,b,a#,a,g,f#,f,e,d#,d,c#,c";
+FAR char *g_song = (FAR char *)
+  "Test:d=4,o=6,b=90:c,c#,d,d#,e,f,f#,g,a,a#,b,p,b,a#,a,g,f#,f,e,d#,d,c#,c";
+
