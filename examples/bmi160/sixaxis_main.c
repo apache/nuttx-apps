9675e55327b72754df0c04197d232610bc500d06
Alin Jerpelea <alin.jerpelea@sony.com>
2019-11-04 07:28:14 -0600

    apps/examples/bmi160:  Add bmi160 example app.  This app will read the BM160 data and list it on the NSH console.

---
+++ examples/bmi160/sixaxis_main.c
@@ -0,0 +1,105 @@
+/****************************************************************************
+ * examples/bmi160/sixaxis_main.c
+ *
+ *   Copyright 2018 Sony Semiconductor Solutions Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Sony Semiconductor Solutions Corporation nor
+ *    the names of its contributors may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include <nuttx/sensors/bmi160.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define ACC_DEVPATH      "/dev/accel0"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * sixaxis_main
+ ****************************************************************************/
+
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int sixaxis_main(int argc, char *argv[])
+#endif
+{
+  int fd;
+  struct accel_gyro_st_s data;
+  uint32_t prev;
+
+  fd = open(ACC_DEVPATH, O_RDONLY);
+  if (fd < 0)
+    {
+      printf("Device %s open failure. %d\n", ACC_DEVPATH, fd);
+      return -1;
+    }
+
+  prev = 0;
+  for (; ; )
+    {
+      int ret;
+
+      ret = read(fd, &data, sizeof(struct accel_gyro_st_s));
+      if (ret != sizeof(struct accel_gyro_st_s))
+        {
+          fprintf(stderr, "Read failed.\n");
+          break;
+        }
+
+      /* If sensing time has been changed, show 6 axis data. */
+
+      if (prev != data.sensor_time)
+        {
+          printf("[%d] %d, %d, %d / %d, %d, %d\n",
+                 data.sensor_time,
+                 data.gyro.x, data.gyro.y, data.gyro.z,
+                 data.accel.x, data.accel.y, data.accel.z);
+          fflush(stdout);
+          prev = data.sensor_time;
+        }
+    }
+
+  close(fd);
+
+  return 0;
+}
