051722fa3b80fc4e48d885226f7a740381c8b90f
Masayuki Ishikawa <masayuki.ishikawa@gmail.com>
2020-02-10 14:48:53 +0900

    examples: elf: Fix warnings when compiled for RISC-V 64bit

--- examples/elf/tests/mutex/mutex.c
+++ examples/elf/tests/mutex/mutex.c
@@ -64,7 +64,7 @@ static volatile bool bendoftest;
 
 void thread_func(void *parameter)
 {
-  int my_id  = (int)parameter;
+  intptr_t my_id  = (intptr_t)parameter;
   int my_ndx = my_id - 1;
   int i;
 
