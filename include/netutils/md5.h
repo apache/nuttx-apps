23dccb44a7424a929447fba321d482ea744c74c1
Alan C. Assis <engenharia03@siam.ind.br>
2020-04-16 20:52:30 -0300

    Rescue incorrectly removed line

--- include/netutils/md5.h
+++ include/netutils/md5.h
@@ -10,6 +10,8 @@
  *
  *   This code implements the MD5 message-digest algorithm.
  *   The algorithm is due to Ron Rivest.  This code was
+ *   written by Colin Plumb in 1993, no copyright is claimed.
+ *
  *   This code is in the public domain; do with it what you wish.
  *
  *   Equivalent code is available from RSA Data Security, Inc.
