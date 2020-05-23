3a64f305c089eb8dacca30ebcc3210f0e5e8bb36
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-05-23 20:46:57 +0800

    build: Remove the duplicated mksymtab.sh
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- tools/mksymtab.sh
+++ tools/mksymtab.sh
@@ -36,7 +36,7 @@
 
 export LC_ALL=C
 
-usage="Usage: $0 <imagedirpath> <outfilepath>"
+usage="Usage: $0 <imagedirpath> [symtabprefix]"
 
 # Check for the required directory path
 
@@ -48,29 +48,19 @@ if [ -z "$dir" ]; then
   exit 1
 fi
 
-# Get the output file name
+# Get the symbol table prefix
 
-outfile=$2
-if [ -z "$outfile" ]; then
-  echo "ERROR: Missing <outfilepath>"
-  echo ""
-  echo $usage
-  exit 1
-fi
-
-rm -f $outfile
+prefix=$2
 
 # Extract all of the undefined symbols from the ELF files and create a
 # list of sorted, unique undefined variable names.
 
-execlist=`find ${dir} -type f 2>/dev/null`
-if [ ! -z "${execlist}" ]; then
-  for exec in ${execlist}; do
-    nm $exec | fgrep ' U ' | sed -e "s/^[ ]*//g" | cut -d' ' -f2  >>_tmplist
-  done
-
-  varlist=`cat _tmplist | sort - | uniq -`
-  rm -f _tmplist
+varlist=`find $dir -name *-thunk.S 2>/dev/null | xargs grep -h asciz | cut -f3 | sort | uniq`
+if [ -z "$varlist" ]; then
+  execlist=`find $dir -type f -perm -a=x 2>/dev/null`
+  if [ ! -z "$execlist" ]; then
+    varlist=`nm $execlist | fgrep ' U ' | sed -e "s/^[ ]*//g" | cut -d' ' -f2 | sort | uniq`
+  fi
 fi
 
 # Now output the symbol table as a structure in a C source file.  All
@@ -78,34 +68,42 @@ fi
 # any kind of checking for function vs. data objects, then this could
 # failed
 
-echo "#include <nuttx/compiler.h>" >$outfile
-echo "#include <nuttx/symtab.h>" >>$outfile
-echo "" >>$outfile
+echo "#include <nuttx/compiler.h>"
+echo "#include <nuttx/symtab.h>"
+echo ""
 
 for var in $varlist; do
-  echo "extern void *${var};" >>$outfile
+  echo "extern void *${var};"
 done
 
-echo "" >>$outfile
-echo "#if defined(CONFIG_EXECFUNCS_HAVE_SYMTAB)" >>$outfile
-echo "const struct symtab_s CONFIG_EXECFUNCS_SYMTAB_ARRAY[] = " >>$outfile
-echo "#elif defined(CONFIG_SYSTEM_NSH_SYMTAB)" >>$outfile
-echo "const struct symtab_s CONFIG_SYSTEM_NSH_SYMTAB_ARRAYNAME[] = " >>$outfile
-echo "#else" >>$outfile
-echo "const struct symtab_s dummy_symtab[] = " >>$outfile
-echo "#endif" >>$outfile
-echo "{" >>$outfile
+echo ""
+if [ -z "$prefix" ]; then
+  echo "#if defined(CONFIG_EXECFUNCS_HAVE_SYMTAB)"
+  echo "const struct symtab_s CONFIG_EXECFUNCS_SYMTAB_ARRAY[] = "
+  echo "#elif defined(CONFIG_SYSTEM_NSH_SYMTAB)"
+  echo "const struct symtab_s CONFIG_SYSTEM_NSH_SYMTAB_ARRAYNAME[] = "
+  echo "#else"
+  echo "const struct symtab_s dummy_symtab[] = "
+  echo "#endif"
+else
+  echo "const struct symtab_s ${prefix}_exports[] = "
+fi
+echo "{"
 
 for var in $varlist; do
-  echo "  {\"${var}\", &${var}}," >>$outfile
+  echo "  {\"${var}\", &${var}},"
 done
 
-echo "};" >>$outfile
-echo "" >>$outfile
-echo "#if defined(CONFIG_EXECFUNCS_HAVE_SYMTAB)" >>$outfile
-echo "const int CONFIG_EXECFUNCS_NSYMBOLS_VAR = sizeof(CONFIG_EXECFUNCS_SYMTAB_ARRAY) / sizeof(struct symtab_s);" >>$outfile
-echo "#elif defined(CONFIG_SYSTEM_NSH_SYMTAB)" >>$outfile
-echo "const int CONFIG_SYSTEM_NSH_SYMTAB_COUNTNAME = sizeof(CONFIG_SYSTEM_NSH_SYMTAB_ARRAYNAME) / sizeof(struct symtab_s);" >>$outfile
-echo "#else" >>$outfile
-echo "const int dummy_nsymtabs = sizeof(dummy_symtab) / sizeof(struct symtab_s);" >>$outfile
-echo "#endif" >>$outfile
+echo "};"
+echo ""
+if [ -z "$prefix" ]; then
+  echo "#if defined(CONFIG_EXECFUNCS_HAVE_SYMTAB)"
+  echo "const int CONFIG_EXECFUNCS_NSYMBOLS_VAR = sizeof(CONFIG_EXECFUNCS_SYMTAB_ARRAY) / sizeof(struct symtab_s);"
+  echo "#elif defined(CONFIG_SYSTEM_NSH_SYMTAB)"
+  echo "const int CONFIG_SYSTEM_NSH_SYMTAB_COUNTNAME = sizeof(CONFIG_SYSTEM_NSH_SYMTAB_ARRAYNAME) / sizeof(struct symtab_s);"
+  echo "#else"
+  echo "const int dummy_nsymtabs = sizeof(dummy_symtab) / sizeof(struct symtab_s);"
+  echo "#endif"
+else
+  echo "const int ${prefix}_nexports = sizeof(${prefix}_exports) / sizeof(struct symtab_s);"
+fi
