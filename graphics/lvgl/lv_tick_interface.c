ab65ae025bc528c67015cd9626f8d893a9dbfc5e
Matias N <matias@protobits.dev>
2020-10-29 19:46:52 -0300

    lvgl: use malloc/free for memory handling and "tickless" tick interface
    This makes LVGL use malloc/free to handle its memory (instead of
    their own memory handling) and supports a tick interface by which
    LVGL can ask NuttX the elapsed time, instead of having to periodically
    "tick" LVGL internal time in a thread.

---
+++ graphics/lvgl/lv_tick_interface.c
@@ -0,0 +1,73 @@
+/****************************************************************************
+ * graphics/lvgl/lv_tick_interface.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdio.h>
+#include "lv_tick_interface.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Type Declarations
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+uint32_t lv_tick_interface(void)
+{
+  static bool first_time = true;
+  static struct timeval t0;
+
+  if (first_time)
+    {
+      gettimeofday(&t0, NULL);
+      first_time = false;
+      return 0;
+    }
+  else
+    {
+      struct timeval t;
+      struct timeval delta;
+
+      gettimeofday(&t, NULL);
+      timersub(&t, &t0, &delta);
+      return delta.tv_sec * 1000 + delta.tv_usec / 1000;
+    }
+}
