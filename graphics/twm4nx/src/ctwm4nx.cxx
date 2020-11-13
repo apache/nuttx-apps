df09b14f1830e5495ced718984559eb9201b5b96
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-11-13 14:24:20 +0900

    graphics/twm4nx/src/ctwm4nx.cxx: Fix a printf format warning

--- graphics/twm4nx/src/ctwm4nx.cxx
+++ graphics/twm4nx/src/ctwm4nx.cxx
@@ -393,7 +393,7 @@ void CTwm4Nx::genMqName(void)
   unsigned long randvalue =
     (unsigned long)std::random() & 0x00fffffful;
 
-  std::asprintf(&m_queueName, "Twm4Nx%06ul", randvalue);
+  std::asprintf(&m_queueName, "Twm4Nx%06lu", randvalue);
 }
 
 /**
