2e17cd2e6b98e0fc2fe2358830eab02a61b39306
Byron Ellacott <code@bje.id.au>
2021-02-17 20:54:15 +1000

    fsutils: update geometry size (c.f. apache/incubator-nuttx#2861)

--- fsutils/mkfatfs/mkfatfs.c
+++ fsutils/mkfatfs/mkfatfs.c
@@ -154,7 +154,7 @@ static inline int mkfatfs_getgeometry(FAR struct fat_format_s *fmt,
       if (fmt->ff_nsectors > geometry.geo_nsectors)
         {
           ferr("ERROR: User maxblocks (%" PRId32
-               ") exceeds blocks on device (%zu)\n",
+               ") exceeds blocks on device (%" PRIu32 ")\n",
                fmt->ff_nsectors, geometry.geo_nsectors);
 
           return -EINVAL;
