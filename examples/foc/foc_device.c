864a61a4314d0c053d727e4b2e502ed03502bc83
raiden00pl <raiden00@railab.me>
2021-03-03 19:48:19 +0100

    Add FOC motor controller example
    For now, only open-loop velocity control is supported.

---
+++ examples/foc/foc_device.c
@@ -0,0 +1,82 @@
+/****************************************************************************
+ * apps/examples/foc/foc_device.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdio.h>
+#include <fcntl.h>
+
+#include "foc_debug.h"
+#include "foc_device.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: foc_device_open
+ ****************************************************************************/
+
+int foc_device_open(FAR struct foc_device_s *dev, int id)
+{
+  char devpath[32];
+  int  ret   = OK;
+
+  DEBUGASSERT(dev);
+
+  /* Get FOC devpath */
+
+  sprintf(devpath, "%s%d", CONFIG_EXAMPLES_FOC_DEVPATH, id);
+
+  /* Open FOC device */
+
+  dev->fd = open(devpath, 0);
+  if (dev->fd <= 0)
+    {
+      PRINTF("ERROR: open %s failed %d\n", devpath, errno);
+      ret = -errno;
+      goto errout;
+    }
+
+errout:
+  return ret;
+}
+
+/****************************************************************************
+ * Name: foc_device_close
+ ****************************************************************************/
+
+int foc_device_close(FAR struct foc_device_s *dev)
+{
+  int ret = OK;
+
+  DEBUGASSERT(dev);
+
+  if (dev->fd > 0)
+    {
+      close(dev->fd);
+    }
+
+  return ret;
+}
---
+++ examples/foc/foc_mq.h
@@ -0,0 +1,83 @@
+/****************************************************************************
+ * apps/examples/foc/foc_mq.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __EXAMPLES_FOC_FOC_MQ_H
+#define __EXAMPLES_FOC_FOC_MQ_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <mqueue.h>
+
+#include "foc_device.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define CONTROL_MQ_MAXMSG  (10)
+#define CONTROL_MQ_MSGSIZE (5)
+#define CONTROL_MQ_MQNAME  "mqf"
+
+/****************************************************************************
+ * Public Type Definition
+ ****************************************************************************/
+
+/* Thread message type */
+
+enum foc_thr_msg_e
+{
+  CONTROL_MQ_MSG_INVALID  = 0,
+  CONTROL_MQ_MSG_VBUS     = 1,
+  CONTROL_MQ_MSG_APPSTATE = 2,
+  CONTROL_MQ_MSG_VEL      = 3,
+  CONTROL_MQ_MSG_START    = 4,
+  CONTROL_MQ_MSG_KILL     = 5
+};
+
+/* FOC thread handler */
+
+struct foc_mq_s
+{
+  bool     quit;
+  bool     start;
+  int      app_state;
+  uint32_t vbus;
+  uint32_t vel;
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: foc_mq_handle
+ ****************************************************************************/
+
+int foc_mq_handle(mqd_t mq, FAR struct foc_mq_s *h);
+
+#endif /* __EXAMPLES_FOC_FOC_THR_H */
