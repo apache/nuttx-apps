dab398c102dbdebfbc68efdc6e7cdcd8301d17d8
Diego Herranz <diegoherranz@diegoherranz.com>
2021-05-08 08:41:55 +0100

    examples/hdc1008_demo: fix header path
    As recommended by the nxstyle CI check.

--- examples/hdc1008_demo/hdc1008_main.c
+++ examples/hdc1008_demo/hdc1008_main.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * examples/hdc1008_demo/hdc1008_main.c
+ * apps/examples/hdc1008_demo/hdc1008_main.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
