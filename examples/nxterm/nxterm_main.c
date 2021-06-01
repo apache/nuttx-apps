8daa66693bb4a307ca6137f8c4605a472626453f
Xiang Xiao <xiaoxiang@xiaomi.com>
2021-06-01 12:37:18 +0800

    Include assert.h in necessary place
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- examples/nxterm/nxterm_main.c
+++ examples/nxterm/nxterm_main.c
@@ -34,6 +34,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <assert.h>
 #include <errno.h>
 #include <debug.h>
 
--- graphics/pdcurs34/nuttx/pdckbd.c
+++ graphics/pdcurs34/nuttx/pdckbd.c
@@ -45,6 +45,7 @@
 #include <sys/types.h>
 #include <sys/select.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "pdcnuttx.h"
 
