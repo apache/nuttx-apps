ab60df94a2306a9c06c35e2aa3f85d41e8a0a737
Alin Jerpelea <alin.jerpelea@sony.com>
2021-06-09 16:39:27 +0200

    wireless: bluetooth: btsak: update licenses to Apache
    Gregory Nutt is has submitted the SGA
    
    Sebastien Lorquet has submitted the ICLA
    
    as a result we can migrate the licenses to Apache.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- wireless/bluetooth/btsak/btsak_security.c
+++ wireless/bluetooth/btsak/btsak_security.c
@@ -1,6 +1,5 @@
 /****************************************************************************
  * apps/wireless/bluetooth/btsak/btsak_security.c
- * Bluetooth Swiss Army Knife -- Security command
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
