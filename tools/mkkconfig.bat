168fd03848bbca7392529a1e683b1fe66b76e7fe
Alin Jerpelea <alin.jerpelea@sony.com>
2021-06-10 07:37:15 +0200

    tools: update licenses to Apache
    Gregory Nutt is has submitted the SGA
    
    Michal Lyszczek has submitted the ICLA
    
    as a result we can migrate the licenses to Apache.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- tools/mkkconfig.bat
+++ tools/mkkconfig.bat
@@ -2,35 +2,20 @@
 
 REM apps/tools/mkkconfig.bat
 REM
-REM   Copyright (C) 2015-2016 Gregory Nutt. All rights reserved.
-REM   Author: Gregory Nutt <gnutt@nuttx.org>
+REM Licensed to the Apache Software Foundation (ASF) under one or more
+REM contributor license agreements.  See the NOTICE file distributed with
+REM this work for additional information regarding copyright ownership.  The
+REM ASF licenses this file to you under the Apache License, Version 2.0 (the
+REM "License"); you may not use this file except in compliance with the
+REM License.  You may obtain a copy of the License at
 REM
-REM Redistribution and use in source and binary forms, with or without
-REM modification, are permitted provided that the following conditions
-REM are met:
+REM   http://www.apache.org/licenses/LICENSE-2.0
 REM
-REM 1. Redistributions of source code must retain the above copyright
-REM    notice, this list of conditions and the following disclaimer.
-REM 2. Redistributions in binary form must reproduce the above copyright
-REM    notice, this list of conditions and the following disclaimer in
-REM    the documentation and/or other materials provided with the
-REM    distribution.
-REM 3. Neither the name NuttX nor the names of its contributors may be
-REM    used to endorse or promote products derived from this software
-REM    without specific prior written permission.
-REM
-REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-REM "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-REM LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-REM FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-REM COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-REM INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-REM BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-REM OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-REM AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-REM LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-REM ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-REM POSSIBILITY OF SUCH DAMAGE.
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+REM WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+REM License for the specific language governing permissions and limitations
+REM under the License.
 REM
 
 REM Parse command line arguments
