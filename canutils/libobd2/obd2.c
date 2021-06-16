682f1af2f8b08d5677a3792b633b6040cdd25316
Alin Jerpelea <alin.jerpelea@sony.com>
2021-06-16 10:15:54 +0200

    canutils: fix relative path CI error
    error: Path relative to repository other than nuttx must begin with the root directory
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- canutils/libobd2/obd2.c
+++ canutils/libobd2/obd2.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * canutils/libobd2/obd2.c
+ * apps/canutils/libobd2/obd2.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
