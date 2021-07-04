aaf9071722ee2c86da770df9916b92d830fd702f
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2021-07-04 09:13:18 +0900

    graphics/pdcurs34: Fix compile error
    Fix a compile error when CONFIG_SYSTEM_TERMCURSES=y.

--- graphics/pdcurs34/nuttx/pdcdisp.c
+++ graphics/pdcurs34/nuttx/pdcdisp.c
@@ -838,7 +838,7 @@ static void PDC_transform_line_term(FAR SCREEN *s, int lineno, int x,
 
   /* Move to the specified line / col */
 
-  PDC_gotoyx_term(sp, lineno, x);
+  PDC_gotoyx_term(s, lineno, x);
 
   /* Loop through all characters to be displayed */
 
