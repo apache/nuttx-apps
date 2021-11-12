b3452982327c3308a4a45f4abb367e26f190f6a9
Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
2021-11-12 11:04:23 +0100

    system/sched_note/note_main.c: Fix the path of file in the header.
    Signed-off-by: Abdelatif Guettouche <abdelatif.guettouche@espressif.com>

--- system/sched_note/note_main.c
+++ system/sched_note/note_main.c
@@ -1,5 +1,5 @@
 /************************************************************************************
- * apps/system/note/note_main.c
+ * apps/system/sched_note/note_main.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
