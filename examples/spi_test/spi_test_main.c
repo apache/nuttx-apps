82aab3b08a8e5280b3dbd96607f558800bfdb351
Lee Lup Yuen <luppy@appkaki.com>
2021-11-28 20:14:11 +0800

    Adding SPI code

--- examples/spi_test/spi_test_main.c
+++ examples/spi_test/spi_test_main.c
@@ -37,5 +37,32 @@
 int main(int argc, FAR char *argv[])
 {
   printf("Spi_test, World!!\n");
+
+#ifdef TODO
+  /* Open device */
+
+  fd = open(DEV_NAME, O_RDWR);
+  if (fd < 0)
+    {
+      int errcode = errno;
+      printf("ERROR: Failed to open device %s: %d\n", DEV_NAME, errcode);
+      goto errout;
+    }
+
+  /* Configure SPI */
+
+  SPI_LOCK(spi, 1);
+  SPI_SETBITS(spi, 8);
+  SPI_SETMODE(spi, SPIDEV_MODE0);
+  SPI_SETFREQUENCY(spi, CONFIG_LPWAN_SX127X_SPIFREQ);
+
+  /* From sx127x_lock() in https://github.com/lupyuen/incubator-nuttx/blob/master/drivers/wireless/lpwan/sx127x/sx127x.c#L463-L477 */
+
+  /* Close SPI */
+
+  close(fd);
+
+#endif  //  TODO
+
   return 0;
 }
