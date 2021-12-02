36f389b966351b741811f1cdd019e5299ac8b90b
Lee Lup Yuen <luppy@appkaki.com>
2021-12-02 21:05:13 +0800

    Control Chip Select via GPIO

--- examples/spi_test2/spi_test2_main.c
+++ examples/spi_test2/spi_test2_main.c
@@ -23,9 +23,13 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+
+#include <sys/ioctl.h>
 #include <stdio.h>
 #include <assert.h>
 #include <fcntl.h>
+
+#include <nuttx/ioexpander/gpio.h>
 
 /****************************************************************************
  * Public Functions
@@ -39,19 +43,40 @@ int main(int argc, FAR char *argv[])
 {
   printf("Spi_test2, World!!\n");
 
+  /* Open GPIO Output for SPI Chip Select */
+
+  int cs = open("/dev/gpout1", O_RDWR);
+  assert(cs >= 0);
+
   /* Open SPI Test Driver */
 
   int fd = open("/dev/spitest0", O_RDWR);
   assert(fd >= 0);
 
+  /* Set SPI Chip Select to Low */
+
+  int ret = ioctl(cs, GPIOC_WRITE, 0);
+  assert(ret >= 0);
+
   /* Write to SPI Test Driver */
 
-  char data[] = "Hello World";
+  char data[] = "Hello World"; /* TODO */
   int bytes_written = write(fd, data, sizeof(data));
   assert(bytes_written == sizeof(data));
+
+  /* TODO: Read from SPI Test Driver */
+
+  /* Set SPI Chip Select to High */
+
+  ret = ioctl(cs, GPIOC_WRITE, 1);
+  assert(ret >= 0);
 
   /* Close SPI Test Driver */
 
   close(fd);
+
+  /* Close GPIO Output for SPI Chip Select */
+
+  close(cs);
   return 0;
 }
