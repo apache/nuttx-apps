3fda1294d76d271389d7a0b63444ac7685156e22
raiden00pl <raiden00@railab.me>
2021-12-04 17:34:58 +0100

    examples/foc: move setpoint and vbus configuration from foc_adc.h to foc_cfg.h

--- examples/foc/foc_adc.h
+++ examples/foc/foc_adc.h
@@ -31,55 +31,6 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
-/* VBUS source must be specified */
-
-#if defined(CONFIG_EXAMPLES_FOC_VBUS_CONST) &&  \
-  defined(CONFIG_EXAMPLES_FOC_VBUS_ADC)
-#  error
-#endif
-
-/* Velocity source must be specified */
-
-#if defined(CONFIG_EXAMPLES_FOC_SETPOINT_CONST) &&   \
-  defined(CONFIG_EXAMPLES_FOC_SETPOINT_ADC)
-#  error
-#endif
-
-/* VBUS ADC scale factor */
-
-#ifdef CONFIG_EXAMPLES_FOC_VBUS_ADC
-#  define VBUS_ADC_SCALE (CONFIG_EXAMPLES_FOC_ADC_VREF *    \
-                          CONFIG_EXAMPLES_FOC_VBUS_SCALE /  \
-                          CONFIG_EXAMPLES_FOC_ADC_MAX /     \
-                          1000.0f /                         \
-                          1000.0f)
-#endif
-
-/* Velocity ADC scale factor */
-
-#ifdef CONFIG_EXAMPLES_FOC_SETPOINT_ADC
-#  define SETPOINT_ADC_SCALE (1.0f / CONFIG_EXAMPLES_FOC_ADC_MAX)
-#endif
-
-/* If constant velocity is selected, velocity value must be provided */
-
-#ifdef CONFIG_EXAMPLES_FOC_SETPOINT_CONST
-#  define SETPOINT_ADC_SCALE   (1)
-#  if CONFIG_EXAMPLES_FOC_SETPOINT_CONST_VALUE == 0
-#    error
-#  endif
-#endif
-
-/* If constant VBUS is selected, VBUS value must be provided */
-
-#ifdef CONFIG_EXAMPLES_FOC_VBUS_CONST
-#  define VBUS_ADC_SCALE   (1)
-#  define VBUS_CONST_VALUE (CONFIG_EXAMPLES_FOC_VBUS_CONST_VALUE / 1000.0f)
-#  if CONFIG_EXAMPLES_FOC_VBUS_CONST_VALUE == 0
-#    error
-#  endif
-#endif
-
 /* Additional configuration if ADC support is required */
 
 #ifdef CONFIG_EXAMPLES_FOC_HAVE_ADC
