97757c5ed950ed0d19bd77660b7644ab6500b4d5
Lee Lup Yuen <luppy@appkaki.com>
2021-12-14 13:53:08 +0800

    Copy spi_test to sx1262_test

---
+++ examples/sx1262_test/spi_test_main.c
@@ -0,0 +1,57 @@
+/****************************************************************************
+ * apps/examples/spi_test/spi_test_main.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * main
+ ****************************************************************************/
+
+int main(int argc, FAR char *argv[])
+{
+  printf("Spi_test, World!!\n");
+
+  /* Open SPI Test Driver */
+
+  int fd = open("/dev/spitest0", O_RDWR);
+  assert(fd >= 0);
+
+  /* Write to SPI Test Driver */
+
+  static char data[] = "Hello World";
+  int bytes_written = write(fd, data, sizeof(data));
+  assert(bytes_written == sizeof(data));
+
+  /* Close SPI Test Driver */
+
+  close(fd);
+  return 0;
+}
