0cee5ab8c72f347e9e857f5653780afb4b3631aa
Lee Lup Yuen <luppy@appkaki.com>
2021-12-16 01:34:12 +0800

    Clean up

--- examples/sx1262_test/sx1262_test_main.c
+++ examples/sx1262_test/sx1262_test_main.c
@@ -19,6 +19,7 @@
  ****************************************************************************/
 
 //  Demo Program for LoRa SX1262 on NuttX
+#include <nuttx/config.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
