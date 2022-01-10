fade03ad9eeb201c86078698facdc86f9e569a4e
Lee Lup Yuen <luppy@appkaki.com>
2022-01-10 12:09:11 +0800

    Clean up macro

--- examples/rust_test/rust/src/macros.rs
+++ examples/rust_test/rust/src/macros.rs
@@ -4,13 +4,16 @@
 /// Print a formatted message to the serial console
 #[macro_export]
 macro_rules! println {
+    //  If no parameters, print a empty string
     () => {
         $crate::puts_format(format_args!(""))
     };
+    //  If 1 parameter (format string), print the format string
     ($s:expr) => {
-        $crate::puts_format(format_args!(concat!($s, "")))
+        $crate::puts_format(format_args!($s))
     };
+    //  If 2 or more parameters (format string + args), print the formatted output
     ($s:expr, $($tt:tt)*) => {
-        $crate::puts_format(format_args!(concat!($s, ""), $($tt)*))
+        $crate::puts_format(format_args!($s, $($tt)*))
     };
 }
