850607862473be636e2f02dd36fcc779b1bebefe
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-01-23 16:31:43 +0800

    system/adb: Replace printf with syslog since adb is a service
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- system/adb/adb_banner.c
+++ system/adb/adb_banner.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/system/adb/adb_main.c
+ * apps/system/adb/adb_banner.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
