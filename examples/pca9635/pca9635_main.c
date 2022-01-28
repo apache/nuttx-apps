480400c523b2f20fcf65c6e65ba3563a498aa1b9
Petro Karashchenko <petro.karashchenko@gmail.com>
2022-01-28 10:29:13 +0200

    examples/pca9635: change O_RDONLY to O_WRONLY for driver open call
    PCA9635 driver does not support neither need nor write operations.
    The ioctl is supports PWMIOC_SETLED_BRIGHTNESS that is write like
    
    Signed-off-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- examples/pca9635/pca9635_main.c
+++ examples/pca9635/pca9635_main.c
@@ -59,7 +59,7 @@ int main(int argc, FAR char *argv[])
   int fd;
   int ret;
 
-  fd = open(CONFIG_EXAMPLES_PCA9635_DEVNAME, O_RDONLY);
+  fd = open(CONFIG_EXAMPLES_PCA9635_DEVNAME, O_WRONLY);
   if (fd < 0)
     {
       fprintf(stderr, "ERROR: Failed to open %s: %d\n",
