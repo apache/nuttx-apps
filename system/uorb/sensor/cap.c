46c98420b7e11381280effbdf0709d5e2cd0ae00
Jiuzhu Dong <dongjiuzhu1@xiaomi.com>
2022-03-25 19:29:42 +0800

    system/uorb: add builtin sensor topics
    Signed-off-by: Jiuzhu Dong <dongjiuzhu1@xiaomi.com>

---
+++ system/uorb/sensor/cap.c
@@ -0,0 +1,51 @@
+/****************************************************************************
+ * apps/system/uorb/sensor/cap.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <sensor/cap.h>
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+#ifdef CONFIG_DEBUG_UORB
+static void print_sensor_cap_message(FAR const struct orb_metadata *meta,
+                                     FAR const void *buffer)
+{
+  FAR const struct sensor_cap *message = buffer;
+  const orb_abstime now = orb_absolute_time();
+
+  uorbinfo_raw("%s:\ttimestamp: %" PRIu64 " (%" PRIu64 " us ago) "
+               "status: %" PRIu32 " rawdata0: %" PRIu32 " rawdata1: "
+               "%" PRIu32 " rawdata2: %" PRIu32 " rawdata3: %" PRIu32 "",
+               meta->o_name, message->timestamp, now - message->timestamp,
+               message->status, message->rawdata[0], message->rawdata[1],
+               message->rawdata[2], message->rawdata[3]);
+}
+#endif
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+ORB_DEFINE(sensor_cap, struct sensor_cap, print_sensor_cap_message);
