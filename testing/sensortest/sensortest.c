zyfeier/incubator-nuttx-apps

fix: sensor: add sensor type of cap
CHAMPION-162

neededby:811150

Increase the type of cap sensor, including
raw data and detection status.

Change-Id: Ie24e6cb1c8ab571a75dcddc9bdeb4282c9d403b7
Signed-off-by: xiatian6 <xiatian6@xiaomi.com>

testing/sensortest/sensortest.c
@@ -64,6 +64,7 @@ static void print_valb(FAR const char *buffer, FAR const char *name);
 static void print_vali2(FAR const char *buffer, FAR const char *name);
 static void print_ppgd(FAR const char *buffer, FAR const char *name);
 static void print_ppgq(FAR const char *buffer, FAR const char *name);
+static void print_cap(FAR const char *buffer, FAR const char *name);
 static void print_gps(FAR const char *buffer, FAR const char *name);
 static void print_gps_satellite(FAR const char *buffer,
                                 FAR const char *name);
@@ -105,6 +106,7 @@ static const struct sensor_info g_sensor_info[] =
   {print_ppgq,  sizeof(struct sensor_event_ppgq),  "ppgq"},
   {print_valf2, sizeof(struct sensor_event_impd),  "impd"},
   {print_vali2, sizeof(struct sensor_event_ots),   "ots"},
+  {print_cap,   sizeof(struct sensor_event_cap),   "cap"},
   {print_gps_satellite,  sizeof(struct sensor_event_gps_satellite),
                                                    "gps_satellite"}
 };
@@ -176,6 +178,16 @@ static void print_ppgq(const char *buffer, const char *name)
          name, event->timestamp, event->ppg[0], event->ppg[1], event->ppg[2],
          event->ppg[3], event->current, event->gain[0], event->gain[1],
          event->gain[2], event->gain[3]);
+}
+
+static void print_cap(FAR const char *buffer, FAR const char *name)
+{
+  struct sensor_event_cap *event = (struct sensor_event_cap *)buffer;
+  printf("%s: timestamp:%" PRIu64 " status:%" PRIu32 " "
+         "rawdata0:%" PRIu32 " rawdata1:%" PRIu32 " rawdata2:%" PRIu32 " "
+         "rawdata3:%" PRIu32 "\n",
+         name, event->timestamp, event->status, event->rawdata[0],
+         event->rawdata[1], event->rawdata[2], event->rawdata[3]);
 }
 
 static void print_gps(const char *buffer, const char *name)

apache/nuttx-apps

testing/sensortest/sensortest.c
@@ -64,6 +64,7 @@ static void print_valb(FAR const char *buffer, FAR const char *name);
 static void print_vali2(FAR const char *buffer, FAR const char *name);
 static void print_ppgd(FAR const char *buffer, FAR const char *name);
 static void print_ppgq(FAR const char *buffer, FAR const char *name);
+static void print_cap(FAR const char *buffer, FAR const char *name);
 static void print_gps(FAR const char *buffer, FAR const char *name);
 static void print_gps_satellite(FAR const char *buffer,
                                 FAR const char *name);
@@ -105,6 +106,7 @@ static const struct sensor_info g_sensor_info[] =
   {print_ppgq,  sizeof(struct sensor_ppgq),  "ppgq"},
   {print_valf2, sizeof(struct sensor_impd),  "impd"},
   {print_vali2, sizeof(struct sensor_ots),   "ots"},
+  {print_cap,   sizeof(struct sensor_cap),   "cap"},
   {print_gps_satellite,  sizeof(struct sensor_gps_satellite),
                                               "gps_satellite"}
 };
@@ -176,6 +178,16 @@ static void print_ppgq(const char *buffer, const char *name)
          name, event->timestamp, event->ppg[0], event->ppg[1], event->ppg[2],
          event->ppg[3], event->current, event->gain[0], event->gain[1],
          event->gain[2], event->gain[3]);
+}
+
+static void print_cap(FAR const char *buffer, FAR const char *name)
+{
+  struct sensor_cap *event = (struct sensor_cap *)buffer;
+  printf("%s: timestamp:%" PRIu64 " status:%" PRIu32 " "
+         "rawdata0:%" PRIu32 " rawdata1:%" PRIu32 " rawdata2:%" PRIu32 " "
+         "rawdata3:%" PRIu32 "\n",
+         name, event->timestamp, event->status, event->rawdata[0],
+         event->rawdata[1], event->rawdata[2], event->rawdata[3]);
 }
 
 static void print_gps(const char *buffer, const char *name)