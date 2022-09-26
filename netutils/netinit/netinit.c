c90d9de98a4f6c101c42330e7dc95231ab59d1bf
chao an <anchao@xiaomi.com>
2022-09-27 02:58:16 +0800

    netinit: associate wlan if DRIVERS_IEEE80211 enabled
    Signed-off-by: chao an <anchao@xiaomi.com>

--- netutils/netinit/netinit.c
+++ netutils/netinit/netinit.c
@@ -595,7 +595,7 @@ static void netinit_net_bringup(void)
       return;
     }
 
-#ifdef CONFIG_WIRELESS_WAPI
+#if defined(CONFIG_WIRELESS_WAPI) && defined(CONFIG_DRIVERS_IEEE80211)
   /* Associate the wlan with an access point. */
 
   if (netinit_associate(NET_DEVNAME) < 0)
