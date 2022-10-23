3841fb5c85f8f2e8999ef6c14341b3d99d1eda45
Petro Karashchenko <petro.karashchenko@gmail.com>
2022-10-23 09:28:49 +0300

    Revert "netutils/thttpd: remove MIN and MAX definitions from libhttpd.h"
    This reverts commit c52f40770e5368980e334048423f5a46fe1f4fef.

--- examples/nunchuck/nunchuck_main.c
+++ examples/nunchuck/nunchuck_main.c
@@ -55,7 +55,6 @@
 #ifndef MIN
 #  define MIN(a,b) ((a) < (b) ? (a) : (b))
 #endif
-
 #ifndef MAX
 #  define MAX(a,b) ((a) > (b) ? (a) : (b))
 #endif
--- examples/zerocross/zerocross_main.c
+++ examples/zerocross/zerocross_main.c
@@ -56,7 +56,6 @@
 #ifndef MIN
 #  define MIN(a,b) ((a) < (b) ? (a) : (b))
 #endif
-
 #ifndef MAX
 #  define MAX(a,b) ((a) > (b) ? (a) : (b))
 #endif
