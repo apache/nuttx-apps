e12d48f0d240edbd6934aac373242fe6b9b9aad2
Petro Karashchenko <petro.karashchenko@gmail.com>
2022-10-23 09:28:49 +0300

    Revert "netutils/thttpd: remove MIN and MAX definitions from libhttpd.h"
    This reverts commit c52f40770e5368980e334048423f5a46fe1f4fef.

--- netutils/thttpd/libhttpd.c
+++ netutils/thttpd/libhttpd.c
@@ -95,6 +95,7 @@ extern CODE char *crypt(const char *key, const char *setting);
 #ifndef MAX
 #  define MAX(a,b) ((a) > (b) ? (a) : (b))
 #endif
+
 #ifndef MIN
 #  define MIN(a,b) ((a) < (b) ? (a) : (b))
 #endif
