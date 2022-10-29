91dfa5f2ff37bdfcf48fcc9761a67985fc148bda
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-10-29 18:49:34 +0800

    Fix Error: keyboard_main.c:420:15: error: result of comparison of constant -1 with expression of type 'char' is always true [-Werror,-Wtautological-constant-out-of-range-compare]       if (key != EOF)           ~~~ ^  ~~~
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- examples/fmsynth/keyboard_main.c
+++ examples/fmsynth/keyboard_main.c
@@ -372,7 +372,7 @@ int main(int argc, FAR char *argv[])
 {
   int i;
   int ret;
-  char key;
+  int key;
   bool running = true;
   pthread_t pid;
   struct app_options appopt;
@@ -416,7 +416,7 @@ int main(int argc, FAR char *argv[])
 
   while (running)
     {
-      key = (char)getchar();
+      key = getchar();
       if (key != EOF)
         {
           switch (key)
