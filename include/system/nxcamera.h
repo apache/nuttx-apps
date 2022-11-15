8bb86e38bd5ba685449dccff5288f7ddeb14c47b
Peter Bee <bijunda1@xiaomi.com>
2022-11-15 18:25:31 +0800

    apps/system: add nxcamera app
    Add NxCamera app similar to NxLooper, which can be used to control video
    capture device. The basic capture n' display function is complete, while
    file output and camera control features are left TODO.
    
    Signed-off-by: Peter Bee <bijunda1@xiaomi.com>

---
+++ include/system/nxcamera.h
@@ -0,0 +1,254 @@
+/****************************************************************************
+ * apps/include/system/nxcamera.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __APPS_INCLUDE_SYSTEM_NXCAMERA_H
+#define __APPS_INCLUDE_SYSTEM_NXCAMERA_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/video/video.h>
+#include <nuttx/video/fb.h>
+#include <mqueue.h>
+#include <pthread.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Standard Video Message Queue message IDs */
+
+#define VIDEO_MSG_NONE              0
+#define VIDEO_MSG_STOP              1
+
+/****************************************************************************
+ * Public Type Declarations
+ ****************************************************************************/
+
+/* This structure describes the internal state of the nxcamera */
+
+struct nxcamera_s
+{
+  int                   loopstate;                   /* Current looper test state */
+  int                   capture_fd;                  /* File descriptor of active
+                                                      * capture device */
+  char                  capturedev[CONFIG_NAME_MAX]; /* Preferred capture device */
+  int                   display_fd;                  /* File descriptor of active
+                                                      * display device */
+  char                  displaydev[CONFIG_NAME_MAX]; /* Display framebuffer device */
+  struct fb_planeinfo_s display_pinfo;               /* Display plane info */
+  char                  videopath[CONFIG_NAME_MAX];  /* Output video file path */
+  char                  imagepath[CONFIG_NAME_MAX];  /* Output image file path */
+  int                   crefs;                       /* Number of references */
+  pthread_mutex_t       mutex;                       /* Thread sync mutex */
+  char                  mqname[14];                  /* Name of display message queue */
+  mqd_t                 mq;                          /* Message queue for the
+                                                      * loopthread */
+  pthread_t             loop_id;                     /* Thread ID of the loopthread */
+  v4l2_format_t         fmt;                         /* Buffer format */
+  size_t                nbuffers;                    /* Number of buffers */
+  FAR size_t            *buf_sizes;                  /* Buffer lengths */
+  FAR uint8_t           **bufs;                      /* Buffer pointers */
+};
+
+struct video_msg_s
+{
+  uint16_t      msg_id;       /* Message ID */
+  union
+  {
+    FAR void    *ptr;         /* Buffer being dequeued */
+    uint32_t    data;         /* Message data */
+  } u;
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: nxcamera_create
+ *
+ *   Allocates and Initializes a nxcamera context that is passed to all
+ *   nxcamera routines.  The looper MUST be destroyed using the
+ *   nxcamera_destroy() routine since the context is reference counted.
+ *   The context can be used in a mode where the caller creates the
+ *   context, starts a looping, and then forgets about the context
+ *   and it will self free.  This is because the nxcamera_stream
+ *   will also create a reference to the context, so the client calling
+ *   nxcamera_destroy() won't actually de-allocate anything. The freeing
+ *   will occur after the loopthread has completed.
+ *
+ *   Alternately, the caller can create the object and hold on to it, then
+ *   the context will persist until the original creator destroys it.
+ *
+ * Input Parameters:    None
+ *
+ * Returned Value:
+ *   Pointer to created nxcamera context or NULL if error.
+ ****************************************************************************/
+
+FAR struct nxcamera_s *nxcamera_create(void);
+
+/****************************************************************************
+ * Name: nxcamera_release
+ *
+ *   Reduces the reference count to the looper and if it reaches zero,
+ *   frees all memory used by the context.
+ *
+ * Input Parameters:
+ *   pcam    Pointer to the nxcamera context
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+void nxcamera_release(FAR struct nxcamera_s *pcam);
+
+/****************************************************************************
+ * Name: nxcamera_reference
+ *
+ *   Increments the reference count to the looper.
+ *
+ * Input Parameters:
+ *   pcam    Pointer to the nxcamera context
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+void nxcamera_reference(FAR struct nxcamera_s *pcam);
+
+/****************************************************************************
+ * Name: nxcamera_stream
+ *
+ *   nxcamera_stream() tries to capture and then display the raw data using
+ *   the Video system. If a capture device is specified, it will try to use
+ *   that device.
+ *
+ * Input:
+ *   pcam       Pointer to the initialized Looper context
+ *   width      Capture frame width
+ *   height     Capture frame height
+ *   framerate  Capture frame rate
+ *   format     Capture frame pixel format
+ *
+ * Returns:
+ *   OK         Video is being streamed
+ *   -EBUSY     Capture device is busy
+ *   -ENOSYS    No supported video format found
+ *   -ENODEV    No video capture or framebuffer device suitable
+ *
+ ****************************************************************************/
+
+int nxcamera_stream(FAR struct nxcamera_s *pcam,
+                    uint16_t width, uint16_t height,
+                    uint32_t framerate, uint32_t format);
+
+/****************************************************************************
+ * Name: nxcamera_stop
+ *
+ *   Stops current loopback.
+ *
+ * Input Parameters:
+ *   pcam   - Pointer to the context to initialize
+ *
+ * Returned Value:
+ *   OK if file found, device found, and loopback started.
+ *
+ ****************************************************************************/
+
+int nxcamera_stop(FAR struct nxcamera_s *pcam);
+
+/****************************************************************************
+ * Name: nxcamera_setdevice
+ *
+ *   Sets the preferred video device to use with the instance of the
+ *   nxcamera.
+ *
+ * Input Parameters:
+ *   pcam   - Pointer to the context to initialize
+ *   device - Pointer to pathname of the preferred device
+ *
+ * Returned Value:
+ *   OK if context initialized successfully, error code otherwise.
+ *
+ ****************************************************************************/
+
+int nxcamera_setdevice(FAR struct nxcamera_s *pcam,
+                       FAR const char *device);
+
+/****************************************************************************
+ * Name: nxcamera_setfb
+ *
+ *   Sets the output framebuffer device to use with the
+ *   provided nxcamera context.
+ *
+ * Input Parameters:
+ *   pcam   - Pointer to the context to initialize
+ *   device - Pointer to pathname of the preferred framebuffer device
+ *
+ * Returned Value:
+ *   OK if context initialized successfully, error code otherwise.
+ *
+ ****************************************************************************/
+
+int nxcamera_setfb(FAR struct nxcamera_s *pcam, FAR const char *device);
+
+/****************************************************************************
+ * Name: nxcamera_setfile
+ *
+ *   Sets the output file path to use with the provided nxcamera context.
+ *
+ * Input Parameters:
+ *   pcam      - Pointer to the context to initialize
+ *   file      - Pointer to pathname of the preferred output file
+ *   isimage   - The path is for image file or not
+ *
+ * Returned Value:
+ *   OK if file found, device found, and loopback started.
+ *
+ ****************************************************************************/
+
+int nxcamera_setfile(FAR struct nxcamera_s *pcam, FAR const char *pfile,
+                     bool isimage);
+
+#undef EXTERN
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __APPS_INCLUDE_SYSTEM_NXCAMERA_H */
---
+++ system/nxcamera/nxcamera_main.c
@@ -0,0 +1,496 @@
+/****************************************************************************
+ * apps/system/nxcamera/nxcamera_main.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/video/video.h>
+
+#include <sys/types.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <system/readline.h>
+#include <system/nxcamera.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define NXCAMERA_VER    "1.00"
+
+#ifdef CONFIG_NXCAMERA_INCLUDE_HELP
+#  define NXCAMERA_HELP_TEXT(x)  x
+#else
+#  define NXCAMERA_HELP_TEXT(x)
+#endif
+
+/****************************************************************************
+ * Private Type Declarations
+ ****************************************************************************/
+
+typedef CODE int (*nxcamera_func)(FAR struct nxcamera_s *cam, FAR char *arg);
+
+struct nxcamera_cmd_s
+{
+  FAR const char *cmd;       /* The command text */
+  FAR const char *arghelp;   /* Text describing the args */
+  nxcamera_func  pfunc;      /* Pointer to command handler */
+  FAR const char *help;      /* The help text */
+};
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+static int nxcamera_cmd_quit(FAR struct nxcamera_s *pcam, FAR char *parg);
+static int nxcamera_cmd_stream(FAR struct nxcamera_s *pcam, FAR char *parg);
+static int nxcamera_cmd_input(FAR struct nxcamera_s *pcam, FAR char *parg);
+static int nxcamera_cmd_output(FAR struct nxcamera_s *pcam, FAR char *parg);
+static int nxcamera_cmd_stop(FAR struct nxcamera_s *pcam, FAR char *parg);
+#ifdef CONFIG_NXCAMERA_INCLUDE_HELP
+static int nxcamera_cmd_help(FAR struct nxcamera_s *pcam, FAR char *parg);
+#endif
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static const struct nxcamera_cmd_s g_nxcamera_cmds[] =
+{
+  {
+    "input",
+    "videodev",
+    nxcamera_cmd_input,
+    NXCAMERA_HELP_TEXT("Specify a preferred capture device")
+  },
+  {
+    "output",
+    "fbdev|filename",
+    nxcamera_cmd_output,
+    NXCAMERA_HELP_TEXT("Specify a display device or filename")
+  },
+#ifdef CONFIG_NXCAMERA_INCLUDE_HELP
+  {
+    "h",
+    "",
+    nxcamera_cmd_help,
+    NXCAMERA_HELP_TEXT("Display help for commands")
+  },
+  {
+    "help",
+    "",
+    nxcamera_cmd_help,
+    NXCAMERA_HELP_TEXT("Display help for commands")
+  },
+#endif
+  {
+    "stream",
+    "width height framerate format",
+    nxcamera_cmd_stream,
+    NXCAMERA_HELP_TEXT("Video stream test (format is fourcc)")
+  },
+  {
+    "stop",
+    "",
+    nxcamera_cmd_stop,
+    NXCAMERA_HELP_TEXT("Stop stream")
+  },
+  {
+    "q",
+    "",
+    nxcamera_cmd_quit,
+    NXCAMERA_HELP_TEXT("Exit NxCamera")
+  },
+  {
+    "quit",
+    "",
+    nxcamera_cmd_quit,
+    NXCAMERA_HELP_TEXT("Exit NxCamera")
+  }
+};
+
+static const int g_nxcamera_cmd_count = sizeof(g_nxcamera_cmds) /
+                                        sizeof(struct nxcamera_cmd_s);
+
+/****************************************************************************
+ * Name: nxcamera_cmd_stream
+ *
+ *   nxcamera_cmd_loop() play and record the raw data file using the nxcamera
+ *   context.
+ *
+ ****************************************************************************/
+
+static int nxcamera_cmd_stream(FAR struct nxcamera_s *pcam, FAR char *parg)
+{
+  uint16_t width = 0;
+  uint16_t height = 0;
+  uint32_t framerate = 0;
+  uint32_t format = 0;
+  int      ret;
+  char     cc[4] =
+    {
+      0
+    };
+
+  sscanf(parg, "%hd %hd %d %s", &width, &height, &framerate, cc);
+  format = v4l2_fourcc(cc[0], cc[1], cc[2], cc[3]);
+
+  /* Try to stream raw data with settings specified */
+
+  ret = nxcamera_stream(pcam, width, height, framerate, format);
+
+  /* nxcamera_stream returned values:
+   *
+   *   OK         Stream is being run
+   *   -EBUSY     The media device is busy
+   *   -ENOSYS    The video format is not unsupported
+   *   -ENODEV    No video device suitable to capture media
+   */
+
+  switch (ret)
+    {
+      case OK:
+        break;
+
+      case -ENODEV:
+        printf("No suitable Video Device found\n");
+        break;
+
+      case -EBUSY:
+        printf("Video device busy\n");
+        break;
+
+      case -ENOSYS:
+        printf("Unknown video format\n");
+        break;
+
+      default:
+        printf("Error stream test: %d\n", ret);
+        break;
+    }
+
+  return ret;
+}
+
+/****************************************************************************
+ * Name: nxcamera_cmd_stop
+ *
+ *   nxcamera_cmd_stop() stops stream context.
+ *
+ ****************************************************************************/
+
+static int nxcamera_cmd_stop(FAR struct nxcamera_s *pcam, FAR char *parg)
+{
+  /* Stop the stream */
+
+  return nxcamera_stop(pcam);
+}
+
+/****************************************************************************
+ * Name: nxcamera_cmd_input
+ *
+ *   nxcamera_cmd_input() sets the preferred capture device for stream.
+ *
+ ****************************************************************************/
+
+static int nxcamera_cmd_input(FAR struct nxcamera_s *pcam, FAR char *parg)
+{
+  int  ret;
+  char path[PATH_MAX];
+
+  /* First try to open the file directly */
+
+  ret = nxcamera_setdevice(pcam, parg);
+  if (ret < 0)
+    {
+      /* Append the /dev path and try again */
+
+      snprintf(path, sizeof(path), "/dev/%s", parg);
+      ret = nxcamera_setdevice(pcam, path);
+    }
+
+  /* Test if the device file exists */
+
+  if (ret == -ENOENT)
+    {
+      /* Device doesn't exit.  Report error */
+
+      printf("Device %s not found\n", parg);
+    }
+  else if (ret == -ENODEV)
+    {
+      /* Test if is is an video device */
+
+      printf("Device %s is not an video device\n", parg);
+    }
+
+  /* Return error value */
+
+  return ret;
+}
+
+/****************************************************************************
+ * Name: nxcamera_cmd_output
+ *
+ *   nxcamera_cmd_device() sets the output device/file for display/save.
+ *
+ ****************************************************************************/
+
+static int nxcamera_cmd_output(FAR struct nxcamera_s *pcam, FAR char *parg)
+{
+  int      ret;
+  char     path[PATH_MAX];
+  FAR char *ext;
+  bool     isimage;
+
+  /* First try to open the device directly */
+
+  ret = nxcamera_setfb(pcam, parg);
+  if (ret < 0)
+    {
+      /* Append the /dev path and try again */
+
+      snprintf(path, sizeof(path), "/dev/%s", parg);
+      ret = nxcamera_setfb(pcam, path);
+    }
+
+  /* Device doesn't exist or is not a video device. Treat as file */
+
+  if (ret < 0)
+    {
+      if (ret == -ENODEV)
+        {
+          printf("Device %s is not an video device\n", parg);
+          return ret;
+        }
+
+      ext = strrchr(parg, '.');
+      if (ext && (ext != parg))
+        {
+          ext++;
+          isimage = strncmp(ext, "jpg", sizeof("jpg")) == 0 ||
+                    strncmp(ext, "jpeg", sizeof("jpeg")) == 0;
+        }
+
+      ret = nxcamera_setfile(pcam, parg, isimage);
+    }
+
+  if (ret < 0)
+    {
+      /* Create file error. Report error */
+
+      printf("Error outputting to %s\n", parg);
+    }
+
+  /* Output device or file set successfully */
+
+  return ret;
+}
+
+/****************************************************************************
+ * Name: nxcamera_cmd_quit
+ *
+ *   nxcamera_cmd_quit() terminates the application.
+ *
+ ****************************************************************************/
+
+static int nxcamera_cmd_quit(FAR struct nxcamera_s *pcam, FAR char *parg)
+{
+  /* Stop the stream if any */
+
+  return nxcamera_stop(pcam);
+}
+
+/****************************************************************************
+ * Name: nxcamera_cmd_help
+ *
+ *   nxcamera_cmd_help() displays the application's help information on
+ *   supported commands and command syntax.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_NXCAMERA_INCLUDE_HELP
+static int nxcamera_cmd_help(FAR struct nxcamera_s *pcam, FAR char *parg)
+{
+  int len;
+  int maxlen = 0;
+  int x;
+  int c;
+
+  /* Calculate length of longest cmd + arghelp */
+
+  for (x = 0; x < g_nxcamera_cmd_count; x++)
+    {
+      len = strlen(g_nxcamera_cmds[x].cmd) +
+            strlen(g_nxcamera_cmds[x].arghelp);
+      if (len > maxlen)
+        {
+          maxlen = len;
+        }
+    }
+
+  printf("NxCamera commands\n================\n");
+  for (x = 0; x < g_nxcamera_cmd_count; x++)
+    {
+      /* Print the command and it's arguments */
+
+      printf("  %s %s", g_nxcamera_cmds[x].cmd, g_nxcamera_cmds[x].arghelp);
+
+      /* Calculate number of spaces to print before the help text */
+
+      len = maxlen - (strlen(g_nxcamera_cmds[x].cmd) +
+                      strlen(g_nxcamera_cmds[x].arghelp));
+      for (c = 0; c < len; c++)
+        {
+          printf(" ");
+        }
+
+      printf("  : %s\n", g_nxcamera_cmds[x].help);
+    }
+
+  return OK;
+}
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: nxcamera
+ *
+ *   nxcamera reads in commands from the console using the readline
+ *   system add-in and impalements a command-line based media loop
+ *   tester that uses the NuttX video system to capture and then display
+ *   or save video from the lower video driver. Commands are provided for
+ *   setting width, height, framerate, pixel format and other video controls,
+ *   as well as for stopping the test.
+ *
+ ****************************************************************************/
+
+int main(int argc, FAR char *argv[])
+{
+  char                  buffer[CONFIG_NSH_LINELEN];
+  int                   len;
+  int                   x;
+  bool                  running = true;
+  FAR char              *cmd;
+  FAR char              *arg;
+  FAR struct nxcamera_s *pcam;
+
+  printf("NxCamera version " NXCAMERA_VER "\n");
+  printf("h for commands, q to exit\n");
+#ifndef CONFIG_LIBYUV
+  printf("Libyuv is not enabled, won't output to RGB framebuffer\n");
+#endif
+  printf("\n");
+
+  /* Initialize our NxCamera context */
+
+  pcam = nxcamera_create();
+  if (pcam == NULL)
+    {
+      printf("Error: Out of RAM\n");
+      return -ENOMEM;
+    }
+
+  /* Loop until the user exits */
+
+  while (running)
+    {
+      /* Print a prompt */
+
+      printf("nxcamera> ");
+      fflush(stdout);
+
+      /* Read a line from the terminal */
+
+      len = readline(buffer, sizeof(buffer), stdin, stdout);
+      if (len > 0)
+        {
+          buffer[len] = '\0';
+          if (strncmp(buffer, "!", 1) != 0)
+            {
+              /* nxcamera command */
+
+              if (buffer[len - 1] == '\n')
+                {
+                  buffer[len - 1] = '\0';
+                }
+
+              /* Parse the command from the argument */
+
+              cmd = strtok_r(buffer, " \n", &arg);
+              if (cmd == NULL)
+                {
+                  continue;
+                }
+
+              /* Find the command in our cmd array */
+
+              for (x = 0; x < g_nxcamera_cmd_count; x++)
+                {
+                  if (strcmp(cmd, g_nxcamera_cmds[x].cmd) == 0)
+                    {
+                      /* Command found.  Call it's handler if not NULL */
+
+                      if (g_nxcamera_cmds[x].pfunc != NULL)
+                        {
+                          g_nxcamera_cmds[x].pfunc(pcam, arg);
+                        }
+
+                      /* Test if it is a quit command */
+
+                      if (g_nxcamera_cmds[x].pfunc == nxcamera_cmd_quit)
+                        {
+                          running = false;
+                        }
+
+                      break;
+                    }
+                }
+            }
+          else
+            {
+#ifdef CONFIG_SYSTEM_SYSTEM
+              /* Transfer nuttx shell */
+
+              system(buffer + 1);
+#else
+              printf("%s: unknown nxcamera command\n", buffer);
+#endif
+            }
+        }
+    }
+
+  /* Release the NxCamera context */
+
+  nxcamera_release(pcam);
+
+  return OK;
+}
