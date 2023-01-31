fa26bab6b24626a47af8886aaf518df1d00ce9ba
Peter van der Perk <peter.vanderperk@nxp.com>
2023-01-31 16:35:23 +0100

    DroneCAN add socketcan support, rename to DroneCAN
    Rename canardv1 to OpenCyphal
    
    Apply suggestions from code review
    
    Co-authored-by: Petro Karashchenko <petro.karashchenko@gmail.com>

--- examples/opencyphal/socketcan.c
+++ examples/opencyphal/socketcan.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/examples/canardv1/socketcan.c
+ * apps/examples/opencyphal/socketcan.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
--- examples/opencyphal/socketcan.h
+++ examples/opencyphal/socketcan.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/examples/canardv1/socketcan.h
+ * apps/examples/opencyphal/socketcan.h
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
