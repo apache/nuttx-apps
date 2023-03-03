be6082c927aa513a68c80c7055b2aea6979f860e
simbit18 <101105604+simbit18@users.noreply.github.com>
2023-03-03 15:06:26 +0100

    apps/examples/battery/batt_main.c: Fix mistakes in comments
    Fix mistakes in comments

--- examples/battery/batt_main.c
+++ examples/battery/batt_main.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/examples/batt/batt_main.c
+ * apps/examples/battery/batt_main.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
