b6cd7679e48d3aee48d001f6d6dddaa54fd09bf9
simbit18 <101105604+simbit18@users.noreply.github.com>
2023-03-06 15:59:23 +0100

    apps/examples/pwm/pwm.h: Fix mistakes in comments
    Fix mistakes in comments

--- examples/pwm/pwm.h
+++ examples/pwm/pwm.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/examples/examples/pwm/pwm.h
+ * apps/examples/pwm/pwm.h
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
