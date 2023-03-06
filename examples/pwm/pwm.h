0a7d210dfaaf136ef67d9fc31ae22bf800ea8139
simbit18 <101105604+simbit18@users.noreply.github.com>
2023-03-06 20:12:25 +0100

    Update pwm.h
    Fix nxstyle

--- examples/pwm/pwm.h
+++ examples/pwm/pwm.h
@@ -30,17 +30,23 @@
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
+
 /* Configuration ************************************************************/
+
 /* CONFIG_NSH_BUILTIN_APPS - Build the PWM test as an NSH built-in function.
- * CONFIG_EXAMPLES_PWM_DEVPATH - The path to the PWM device. Default: /dev/pwm0
- * CONFIG_EXAMPLES_PWM_FREQUENCY - The initial PWM frequency.  Default: 100 Hz
- * CONFIG_EXAMPLES_PWM_DUTYPCT - The initial PWM duty as a percentage.  Default: 50%
- * CONFIG_EXAMPLES_PWM_DURATION - The initial PWM pulse train duration in seconds.
- *   Used only if the current pulse count is zero (pulse count is only supported
- *   if CONFIG_PWM_PULSECOUNT is defined). Default: 5 seconds
- * CONFIG_EXAMPLES_PWM_PULSECOUNT - The initial PWM pulse count.  This option is
- *   only available if CONFIG_PWM_PULSECOUNT is defined. Default: 0 (i.e., use
- *   the duration, not the count).
+ * CONFIG_EXAMPLES_PWM_DEVPATH - The path to the PWM device.
+ *   Default: /dev/pwm0
+ * CONFIG_EXAMPLES_PWM_FREQUENCY - The initial PWM frequency.
+ *   Default: 100 Hz
+ * CONFIG_EXAMPLES_PWM_DUTYPCT - The initial PWM duty as a percentage.
+ *   Default: 50%
+ * CONFIG_EXAMPLES_PWM_DURATION - The initial PWM pulse train duration
+ *   in seconds. Used only if the current pulse count is zero
+ *   (pulse count is only supported if CONFIG_PWM_PULSECOUNT is defined).
+ *   Default: 5 seconds
+ * CONFIG_EXAMPLES_PWM_PULSECOUNT - The initial PWM pulse count.
+ *   This option is only available if CONFIG_PWM_PULSECOUNT is defined.
+ *   Default: 0 (i.e., use the duration, not the count).
  */
 
 #ifndef CONFIG_PWM
