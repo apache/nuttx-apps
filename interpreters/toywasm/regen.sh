51d8ffe5001551f415c1de07c5962ecc761f521e
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-03-09 02:56:43 +0900

    toywasm: add a script to regenerate config-dependent files
    This is a manual step mainly because it involves extra
    tools including cmake.

---
+++ interpreters/toywasm/regen.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+# A script to generate a few files for nuttx-apps build.
+
+REPO=https://github.com/yamt/toywasm
+REF=${REF:-master}
+
+TMP=$(mktemp -d)
+trap "rm -rf ${TMP}" 0
+DIR="${TMP}/src"
+BUILDDIR="${TMP}/build"
+
+mkdir -p ${DIR}
+git -C ${DIR} init
+git -C ${DIR} fetch --tags ${REPO} ${REF}
+git -C ${DIR} checkout FETCH_HEAD
+
+# Note: for this build, TOYWASM_USE_SHORT_ENUMS is only used for
+# the "toywasm --version" output.
+
+cmake -B ${BUILDDIR} \
+-DTOYWASM_USE_SHORT_ENUMS=OFF \
+-DTOYWASM_ENABLE_WASM_EXTENDED_CONST=ON \
+-DTOYWASM_ENABLE_WASM_MULTI_MEMORY=ON \
+-DTOYWASM_ENABLE_WASM_TAILCALL=ON \
+-DTOYWASM_ENABLE_WASM_THREADS=ON \
+-DTOYWASM_ENABLE_WASI_THREADS=ON \
+${DIR}
+
+for fn in \
+    include/toywasm_config.h \
+    include/toywasm_version.h \
+    src/toywasm_config.c; do
+
+    (m4 -DFILENAME=$fn tmpl/license.in
+    case ${fn} in
+    *.c)
+        cat tmpl/c-sections.in
+        ;;
+    esac
+    # remove comments to appease nxstyle
+    sed -e '/^\/\*/d' ${BUILDDIR}/$(basename $fn)) > ${fn}
+done
