8457ce9db2bae7441ace79195c56bd830e0c2ea3
Zhe Weng <wengzhe@xiaomi.com>
2023-04-07 16:46:19 +0800

    netutils/iperf: Support local socket test
    Usage:
    iperf -s --local <path>
    iperf -c <path> --local || iperf -c <whatever> --local <path>
    
    Can combine with other options, e.g. '-u' will result in local
    UDP (DGRAM).
    
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

--- netutils/iperf/iperf.h
+++ netutils/iperf/iperf.h
@@ -44,10 +44,11 @@ extern "C"
  * Pre-processor Definitions
  ****************************************************************************/
 
-#define IPERF_FLAG_CLIENT (1)
+#define IPERF_FLAG_CLIENT (1 << 0)
 #define IPERF_FLAG_SERVER (1 << 1)
-#define IPERF_FLAG_TCP (1 << 2)
-#define IPERF_FLAG_UDP (1 << 3)
+#define IPERF_FLAG_TCP    (1 << 2)
+#define IPERF_FLAG_UDP    (1 << 3)
+#define IPERF_FLAG_LOCAL  (1 << 4)
 
 /****************************************************************************
  * Public Types
@@ -62,6 +63,7 @@ struct iperf_cfg_t
   uint16_t sport;
   uint32_t interval;
   uint32_t time;
+  FAR const char *path; /* local path or rpmsg name */
 };
 
 /****************************************************************************
