f22dac0b7295a59bd8223d762aaaa4e76fa94cd6
wangchen <wangchen41@xiaomi.com>
2023-05-10 14:58:44 +0800

    nshlib/netcmds:modify the description of setting MTU
    modify the description of setting MTU
    
    Signed-off-by: wangchen <wangchen41@xiaomi.com>

--- nshlib/nsh_command.c
+++ nshlib/nsh_command.c
@@ -255,7 +255,7 @@ static const struct cmdmap_s g_cmdmap[] =
 #ifdef CONFIG_NET
 # ifndef CONFIG_NSH_DISABLE_IFCONFIG
   { "ifconfig", cmd_ifconfig, 1, 12,
-    "[interface [address_family] [<ip-address>|dhcp]] | [mtu <len>]"
+    "[interface [address_family] [mtu <len>] | [<ip-address>|dhcp]]"
     "[dr|gw|gateway <dr-address>] [netmask <net-mask>|prefixlen <len>] "
     "[dns <dns-address>] [hw <hw-mac>]" },
 # endif
