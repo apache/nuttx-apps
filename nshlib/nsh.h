fc8c425cc0607a0e58b3a0978a52ae290ec916ed
wangjianyu3 <wangjianyu3@xiaomi.com>
2023-07-04 17:08:20 +0800

    Fix dependents of nsh_foreach_direntry Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- nshlib/nsh.h
+++ nshlib/nsh.h
@@ -571,9 +571,14 @@
 #  undef NSH_HAVE_READFILE
 #endif
 
-/* nsh_foreach_direntry used by the ls and ps and fdinfo commands */
+/* nsh_foreach_direntry used by the commands:
+ * ls, ps, fdinfo, rptun, pmconfig
+ */
 
-#if defined(CONFIG_NSH_DISABLE_LS) && defined(CONFIG_NSH_DISABLE_PS) && defined(CONFIG_NSH_DISABLE_FDINFO)
+#if defined(CONFIG_NSH_DISABLE_LS) && defined(CONFIG_NSH_DISABLE_PS) && \
+    defined(CONFIG_NSH_DISABLE_FDINFO) && \
+    defined(CONFIG_NSH_DISABLE_RPTUN) && \
+    defined(CONFIG_NSH_DISABLE_PMCONFIG)
 #  undef NSH_HAVE_FOREACH_DIRENTRY
 #endif
 
