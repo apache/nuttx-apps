4e016e226064aabaee1e2fa7630b2dd2a3ad1383
makejian <makejian@xiaomi.com>
2023-07-18 21:44:38 +0800

    mbedtls: add nuttx crypto alternative implementation in mbedtls
    (1)add dev_alt to manage /dev/crypto
    (2)add sha_alt to alternate sha1 algorithm
    (3)use new mbedtls_config.h to admin configs of mbedtls
    Signed-off-by: makejian <makejian@xiaomi.com>

---
+++ crypto/mbedtls/include/dev_alt.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+ * apps/crypto/mbedtls/include/dev_alt.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ ****************************************************************************/
+
+#ifndef __APPS_CRYPTO_MBEDTLS_INCLUDE_DEV_ALT_H
+#define __APPS_CRYPTO_MBEDTLS_INCLUDE_DEV_ALT_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <crypto/cryptodev.h>
+
+typedef struct cryptodev_context_s
+{
+  int fd;
+  struct session_op session;
+  struct crypt_op crypt;
+}
+cryptodev_context_t;
+
+int cryptodev_init(FAR cryptodev_context_t *ctx);
+int cryptodev_clone(FAR cryptodev_context_t *dst,
+                    FAR const cryptodev_context_t *src);
+void cryptodev_free(FAR cryptodev_context_t *ctx);
+int cryptodev_get_session(FAR cryptodev_context_t *ctx);
+void cryptodev_free_session(FAR cryptodev_context_t *ctx);
+int cryptodev_crypt(FAR cryptodev_context_t *ctx);
+
+#endif /* __APPS_CRYPTO_MBEDTLS_INCLUDE_DEV_ALT_H */
--- crypto/mbedtls/source/sha1_alt.c
+++ crypto/mbedtls/source/sha1_alt.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/include/crypto/mbedtls_config.h
+ * apps/crypto/mbedtls/source/sha1_alt.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,68 +15,60 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations
  * under the License.
- *
  ****************************************************************************/
-
-#ifndef __APPS_INCLUDE_CRYPTO_MBEDTLS_CONFIG_H
-#define __APPS_INCLUDE_CRYPTO_MBEDTLS_CONFIG_H
 
 /****************************************************************************
  * Included Files
  ****************************************************************************/
 
-#include <nuttx/config.h>
+#include "mbedtls/sha1.h"
 
 /****************************************************************************
- * Pre-processor Definitions
+ * Public Functions
  ****************************************************************************/
 
-/* System support */
-#define MBEDTLS_HAVE_TIME
+void mbedtls_sha1_clone(FAR mbedtls_sha1_context *dst,
+                        FAR const mbedtls_sha1_context *src)
+{
+  cryptodev_clone(dst, src);
+}
 
-/* Debug */
-#define MBEDTLS_SELF_TEST
-#define MBEDTLS_TIMING_C
+void mbedtls_sha1_init(FAR mbedtls_sha1_context *ctx)
+{
+  cryptodev_init(ctx);
+}
 
-/* Feature support */
-#define MBEDTLS_CIPHER_MODE_CBC
-#define MBEDTLS_PKCS1_V15
-#define MBEDTLS_KEY_EXCHANGE_RSA_ENABLED
-#define MBEDTLS_SSL_PROTO_TLS1_2
+void mbedtls_sha1_free(FAR mbedtls_sha1_context *ctx)
+{
+  cryptodev_free(ctx);
+}
 
-/* Modules */
-#define MBEDTLS_AES_C
-#define MBEDTLS_ASN1_PARSE_C
-#define MBEDTLS_ASN1_WRITE_C
-#define MBEDTLS_BIGNUM_C
-#define MBEDTLS_CIPHER_C
-#define MBEDTLS_CTR_DRBG_C
-#define MBEDTLS_DES_C
-#define MBEDTLS_ENTROPY_C
-#define MBEDTLS_MD_C
-#define MBEDTLS_MD5_C
+int mbedtls_sha1_starts(FAR mbedtls_sha1_context *ctx)
+{
+  ctx->session.mac = CRYPTO_SHA1;
+  return cryptodev_get_session(ctx);
+}
 
-#ifdef CONFIG_NET
-#define MBEDTLS_NET_C
-#endif
+int mbedtls_sha1_update(FAR mbedtls_sha1_context *ctx,
+                        FAR const unsigned char *input,
+                        size_t ilen)
+{
+  ctx->crypt.op = COP_ENCRYPT;
+  ctx->crypt.flags |= COP_FLAG_UPDATE;
+  ctx->crypt.src = (caddr_t)input;
+  ctx->crypt.len = ilen;
+  return cryptodev_crypt(ctx);
+}
 
-#define MBEDTLS_OID_C
-#define MBEDTLS_PK_C
-#define MBEDTLS_PK_PARSE_C
-#define MBEDTLS_RSA_C
-#define MBEDTLS_SHA1_C
-#define MBEDTLS_SHA224_C
-#define MBEDTLS_SHA256_C
-#define MBEDTLS_SSL_CLI_C
-#define MBEDTLS_SSL_SRV_C
-#define MBEDTLS_SSL_TLS_C
-#define MBEDTLS_X509_CRT_PARSE_C
-#define MBEDTLS_X509_USE_C
+int mbedtls_sha1_finish(FAR mbedtls_sha1_context *ctx,
+                        unsigned char output[20])
+{
+  int ret;
 
-#define MBEDTLS_BASE64_C
-#define MBEDTLS_CERTS_C
-#define MBEDTLS_PEM_PARSE_C
-
-#define MBEDTLS_FS_IO
-
-#endif /* __APPS_INCLUDE_CRYPTO_MBEDTLS_CONFIG_H */
+  ctx->crypt.op = COP_ENCRYPT;
+  ctx->crypt.flags = 0;
+  ctx->crypt.mac = (caddr_t)output;
+  ret = cryptodev_crypt(ctx);
+  cryptodev_free_session(ctx);
+  return ret;
+}
