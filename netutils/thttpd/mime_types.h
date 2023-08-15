7251645685cb72727ba63cc19209b7e13a0c709d
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-08-16 01:41:18 +0800

    Fix some coding style issue
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- netutils/thttpd/mime_types.h
+++ netutils/thttpd/mime_types.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  * apps/netutils/thttpd/mime_types.h
- * Provides mappings between filename extensions and MIME types and encodings.
+ * Provides mappings between filename extensions, MIME types and encodings.
  *
  * Based on mime_encodings.txt and mime_types.txt by Jef Poskanser which
  * contained no copyright information.
--- testing/drivertest/drivertest_audio.c
+++ testing/drivertest/drivertest_audio.c
@@ -27,7 +27,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/time.h>
-#include <sys/params.h>
+#include <sys/param.h>
 
 #include <stdarg.h>
 #include <stddef.h>
