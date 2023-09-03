66651e2191a914524ac2ce63c8d033bb71809c0a
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-09-04 02:35:38 +0800

    system/cu: Merge infd and outfd into devfd
    to avoid opening the same device twice since
    some special device can't work in this case
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- system/cu/cu.h
+++ system/cu/cu.h
@@ -71,8 +71,7 @@
 
 struct cu_globals_s
 {
-  int infd;              /* Incoming data from serial port */
-  int outfd;             /* Outgoing data to serial port */
+  int devfd;             /* I/O data to serial port */
   int stdfd;             /* I/O data to standard console */
   struct termios devtio; /* Original serial port setting */
   struct termios stdtio; /* Original standard console setting */
