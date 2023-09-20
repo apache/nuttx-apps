7fc334cbc4e5cb661943fa9587052070b8f9a459
TaiJu Wu <tjwu1217@gmail.com>
2023-09-20 13:15:45 +0000

    feature: read-write spinlock test
    Signed-off-by: TaiJu Wu <tjwu1217@gmail.com>

--- testing/ostest/spinlock.c
+++ testing/ostest/spinlock.c
@@ -26,12 +26,16 @@
 #include <assert.h>
 #include <errno.h>
 #include <nuttx/spinlock.h>
+#include <stdatomic.h>
 
 #include "ostest.h"
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
+
+#define READER_FLAG (1 << 0)
+#define WRITER_FLAG (1 << 1)
 
 /****************************************************************************
  * Private Data
@@ -42,33 +46,42 @@ static spinlock_t lock = SP_UNLOCKED;
 static pthread_t g_thread1;
 static pthread_t g_thread2;
 
+#ifdef CONFIG_RW_SPINLOCK
+static pthread_t g_thread3;
+static rwlock_t rw_lock = RW_SP_UNLOCKED;
+
+/* To distinguish to write or read (can be 0, 1, 2) */
+
+static int test_rw_flag = 0;
+#endif
+
 static int g_result = 0;
 
-static FAR void thread_spinlock(FAR void *parameter)
+static FAR void *thread_native_spinlock(FAR FAR void *parameter)
 {
-  int pid = *(int *)parameter;
+  int pid = *(FAR int *)parameter;
 
   for (int i = 0; i < 10; i++)
     {
-      printf("pid %d get lock g_result:%d\n", pid, g_result);
+      printf("pid %d try to get lock g_result:%d\n", pid, g_result);
       spin_lock(&lock);
       g_result++;
       spin_unlock(&lock);
       printf("pid %d release lock g_result:%d\n", pid, g_result);
     }
+
+  return NULL;
 }
 
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-void spinlock_test(void)
+static FAR void test_native_spinlock(void)
 {
   int status;
   g_result = 0;
+  lock = SP_UNLOCKED;
+  spin_initialize(&lock, SP_UNLOCKED);
 
   status = pthread_create(&g_thread1, NULL,
-                          (void *)thread_spinlock, &g_thread1);
+                          thread_native_spinlock, &g_thread1);
   if (status != 0)
     {
       printf("spinlock_test: ERROR pthread_create failed, status=%d\n",
@@ -77,7 +90,7 @@ void spinlock_test(void)
     }
 
   status = pthread_create(&g_thread2, NULL,
-                          (void *)thread_spinlock, &g_thread2);
+                          thread_native_spinlock, &g_thread2);
   if (status != 0)
     {
       printf("spinlock_test: ERROR pthread_create failed, status=%d\n",
@@ -89,4 +102,114 @@ void spinlock_test(void)
   pthread_join(g_thread2, NULL);
 
   assert(g_result == 20);
+}
+
+#if defined(CONFIG_RW_SPINLOCK)
+static void FAR *thread_read_spinlock(FAR void *parameter)
+{
+  static atomic_int reader_counter = 0;
+  int pid = *(FAR int *)parameter;
+  int test;
+
+  for (int i = 0; i < 10; ++i)
+    {
+      printf("pid %d try to get read lock g_result:%d\n", pid, g_result);
+      atomic_fetch_add(&reader_counter, 1);
+      read_lock(&rw_lock);
+      test = g_result + 1;
+      test_rw_flag |= READER_FLAG;
+
+      /* Only reader can in critical section */
+
+      ASSERT(test_rw_flag == READER_FLAG);
+      read_unlock(&rw_lock);
+      atomic_fetch_sub(&reader_counter, 1);
+      if (reader_counter == 0)
+        {
+          test_rw_flag &= ~READER_FLAG;
+        }
+      printf("pid %d release read lock g_result+1:%d\n", pid, test);
+    }
+
+  return NULL;
+}
+
+static void FAR *thread_wrt_spinlock(FAR void *parameter)
+{
+  static atomic_int writer_counter = 0;
+  int pid = *(FAR int *)parameter;
+
+  for (int i = 0; i < 10; ++i)
+    {
+      printf("pid %d try to get write lock g_result:%d\n", pid, g_result);
+      atomic_fetch_add(&writer_counter, 1);
+      write_lock(&rw_lock);
+
+      g_result++;
+      test_rw_flag |= WRITER_FLAG;
+
+      /* Only wrtier can in critical section */
+
+      ASSERT(test_rw_flag == WRITER_FLAG && writer_counter == 1);
+      test_rw_flag &= ~WRITER_FLAG;
+      write_unlock(&rw_lock);
+      atomic_fetch_sub(&writer_counter, 1);
+      printf("pid %d release write lock g_result:%d\n", pid, g_result);
+    }
+  write_trylock(&rw_lock);
+  return NULL;
+}
+
+static FAR void test_rw_spinlock(void)
+{
+  int status;
+  g_result = 0;
+  rwlock_init(&rw_lock);
+
+  status = pthread_create(&g_thread1, NULL,
+                          thread_read_spinlock, &g_thread1);
+  if (status != 0)
+    {
+      printf("spinlock_test: ERROR pthread_create failed, status=%d\n",
+              status);
+      ASSERT(false);
+    }
+
+  status = pthread_create(&g_thread2, NULL,
+                          thread_read_spinlock, &g_thread2);
+  if (status != 0)
+    {
+      printf("spinlock_test: ERROR pthread_create failed, status=%d\n",
+              status);
+      ASSERT(false);
+    }
+
+  status = pthread_create(&g_thread3, NULL,
+                          thread_wrt_spinlock, &g_thread3);
+  if (status != 0)
+    {
+      printf("spinlock_test: ERROR pthread_create failed, status=%d\n",
+              status);
+      ASSERT(false);
+    }
+
+  pthread_join(g_thread1, NULL);
+  pthread_join(g_thread2, NULL);
+  pthread_join(g_thread3, NULL);
+
+  assert(g_result == 10);
+}
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+void spinlock_test(void)
+{
+  test_native_spinlock();
+
+#if defined(CONFIG_RW_SPINLOCK)
+  test_rw_spinlock();
+#endif
 }
