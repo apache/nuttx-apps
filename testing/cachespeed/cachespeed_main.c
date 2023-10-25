09bb3b5853b7c77b15a9c8c0fd215e5bf6498b74
chenrun1 <chenrun1@xiaomi.com>
2023-10-25 17:06:44 +0800

    apps:move test tools to testing.
    1.memstress
    2.ramtest
    3.ramspeed
    4.cachespeed
    
    Signed-off-by: chenrun1 <chenrun1@xiaomi.com>

--- testing/cachespeed/cachespeed_main.c
+++ testing/cachespeed/cachespeed_main.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/system/cachespeed/cachespeed_main.c
+ * apps/testing/cachespeed/cachespeed_main.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
