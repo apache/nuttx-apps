cd0914da5ad290b0734ce205c263b09280081613
chenrun1 <chenrun1@xiaomi.com>
2023-11-06 12:17:22 +0800

    apps:Move the performance tools to benchmark.
    1.ramspeed
    2.cachespeed
    3.osperf
    4.iozone
    5.superPI
    
    Signed-off-by: chenrun1 <chenrun1@xiaomi.com>

--- benchmarks/cachespeed/cachespeed_main.c
+++ benchmarks/cachespeed/cachespeed_main.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/testing/cachespeed/cachespeed_main.c
+ * apps/benchmarks/cachespeed/cachespeed_main.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
