9907fde5d4ef7fadb7492df4cc7c8d118864411f
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-11-28 12:41:05 -0300

    examples/rmtchar: Implement RX and/or TX using the RMT char driver
    The RMT (remote control) character driver is used to send and/or
    receive data packets. Eventually, this app can be used to perform
    a loopback test to validate the RMT driver implementation.

---
+++ examples/rmtchar/rmtchar.h
@@ -0,0 +1,127 @@
+/****************************************************************************
+ * apps/examples/rmtchar/rmtchar.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __APPS_EXAMPLES_RMTCHAR_RMTCHAR_H
+#define __APPS_EXAMPLES_RMTCHAR_RMTCHAR_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <pthread.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Configuration */
+
+#ifndef CONFIG_EXAMPLES_RMTCHAR_TX_DEVPATH
+#  define CONFIG_EXAMPLES_RMTCHAR_TX_DEVPATH "/dev/rmt0"
+#endif
+
+#ifndef CONFIG_EXAMPLES_RMTCHAR_RX_DEVPATH
+#  define CONFIG_EXAMPLES_RMTCHAR_RX_DEVPATH "/dev/rmt1"
+#endif
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct rmtchar_state_s
+{
+  bool initialized;
+  int rmtchar_items;
+#ifdef CONFIG_EXAMPLES_RMTCHAR_TX
+  FAR char *txdevpath;
+#endif
+#ifdef CONFIG_EXAMPLES_RMTCHAR_RX
+  FAR char *rxdevpath;
+#endif
+};
+
+struct rmt_item32_s
+{
+  union
+    {
+      struct
+        {
+          uint32_t duration0 : 15; /* Duration of level0 */
+          uint32_t level0 : 1;     /* Level of the first part */
+          uint32_t duration1 : 15; /* Duration of level1 */
+          uint32_t level1 : 1;     /* Level of the second part */
+        };
+      uint32_t val; /* Equivalent unsigned value for the RMT item */
+    };
+};
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: rmtchar_transmitter()
+ *
+ * Description:
+ *   This is the entry point for the transmitter thread.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_EXAMPLES_RMTCHAR_TX
+pthread_addr_t rmtchar_transmitter(pthread_addr_t arg);
+#endif
+
+/****************************************************************************
+ * Name: rmtchar_receiver()
+ *
+ * Description:
+ *   This is the entry point for the receiver thread.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_EXAMPLES_RMTCHAR_RX
+pthread_addr_t rmtchar_receiver(pthread_addr_t arg);
+#endif
+
+/****************************************************************************
+ * Name: print_items
+ *
+ * Description:
+ *   This function prints the level and duration of RMT items stored in a
+ *   buffer. It iterates over the buffer, printing the level and duration of
+ *   each item in a formatted manner.
+ *
+ * Input Parameters:
+ *   buf  - Pointer to the buffer containing the RMT items.
+ *   len  - The number of RMT items in the buffer.
+ *
+ * Returned Value:
+ *   None.
+ *
+ ****************************************************************************/
+
+void print_items(struct rmt_item32_s *buf, int len);
+
+#endif /* __APPS_EXAMPLES_RMTCHAR_RMTCHAR_H */
---
+++ examples/rmtchar/rmtchar_receiver.c
@@ -0,0 +1,138 @@
+/****************************************************************************
+ * apps/examples/rmtchar/rmtchar_receiver.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <errno.h>
+#include <debug.h>
+#include <unistd.h>
+
+#include "rmtchar.h"
+
+#ifdef CONFIG_EXAMPLES_RMTCHAR_RX
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: rmtchar_receiver()
+ *
+ * Description:
+ *   This is the entry point for the receiver thread.
+ *
+ ****************************************************************************/
+
+pthread_addr_t rmtchar_receiver(pthread_addr_t arg)
+{
+  FAR struct rmtchar_state_s *rmtchar = (FAR struct rmtchar_state_s *)arg;
+  struct rmt_item32_s buf[rmtchar->rmtchar_items];
+  int nread;
+  int fd;
+
+  /* Open the RMT character device */
+
+  fd = open(rmtchar->rxdevpath, O_RDONLY);
+  if (fd < 0)
+    {
+      int errcode = errno;
+      printf("rmtchar_receiver: ERROR: failed to open %s: %d\n",
+              rmtchar->rxdevpath, errcode);
+      pthread_exit(NULL);
+    }
+
+  /* Flush any output before reading */
+
+  fflush(stdout);
+
+  /* Read the buffer to the RMT character driver */
+
+  nread = read(fd, (char *)buf, rmtchar->rmtchar_items *
+               sizeof(struct rmt_item32_s));
+  if (nread < 0)
+    {
+      int errcode = errno;
+      if (errcode != EINTR)
+        {
+          printf("rmtchar_receiver: ERROR: read failed: %d\n",
+                  errcode);
+          close(fd);
+          pthread_exit(NULL);
+        }
+    }
+  else
+    {
+      if (nread != (rmtchar->rmtchar_items * sizeof(struct rmt_item32_s)))
+        {
+          printf("rmtchar_receiver: ERROR: partial read: %d\n",
+                  nread);
+          printf("rmtchar_receiver: Received buffer:\n");
+          print_items(buf, nread / sizeof(struct rmt_item32_s));
+          close(fd);
+          pthread_exit(NULL);
+        }
+      else
+        {
+          printf("rmtchar_receiver: Received buffer:\n");
+          print_items(buf, nread / sizeof(struct rmt_item32_s));
+        }
+    }
+
+  /* Make sure that the transmitter thread has a chance to run */
+
+  pthread_yield();
+
+  close(fd);
+  return NULL;
+}
+
+#endif /* CONFIG_EXAMPLES_RMTCHAR_RX */
