1dd8bc4712653b6c460ce25133cc906182aef1c6
anjiahao <anjiahao@xiaomi.com>
2024-04-30 15:41:35 +0800

    sotest & chardev:change elf loading method
    Signed-off-by: anjiahao <anjiahao@xiaomi.com>

--- examples/sotest/modprint/modprint.c
+++ examples/sotest/modprint/modprint.c
@@ -33,23 +33,6 @@
 #include <nuttx/lib/modlib.h>
 
 /****************************************************************************
- * Private Function Prototypes
- ****************************************************************************/
-
-static void modprint(FAR const char *fmt, ...) printf_like(1, 2);
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
-
-static const struct symtab_s g_modprint_exports[1] =
-{
-  {
-    "modprint", (FAR const void *)modprint,
-  }
-};
-
-/****************************************************************************
  * Private Functions
  ****************************************************************************/
 
@@ -57,7 +40,7 @@ static const struct symtab_s g_modprint_exports[1] =
  * Name: modprint
  ****************************************************************************/
 
-static void modprint(FAR const char *fmt, ...)
+visibility_default void modprint(FAR const char *fmt, ...)
 {
   va_list ap;
 
@@ -66,36 +49,3 @@ static void modprint(FAR const char *fmt, ...)
   va_end(ap);
 }
 
-/****************************************************************************
- * Name: module_uninitialize
- ****************************************************************************/
-
-static int module_uninitialize(FAR void *arg)
-{
-  syslog(LOG_INFO, "module_uninitialize: arg=%p\n", arg);
-  return OK;
-}
-
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Name: module_initialize
- *
- * Description:
- *   Register /dev/sotest
- *
- ****************************************************************************/
-
-int module_initialize(FAR struct mod_info_s *modinfo)
-{
-  syslog(LOG_INFO, "module_initialize:\n");
-
-  modinfo->uninitializer = module_uninitialize;
-  modinfo->arg           = NULL;
-  modinfo->exports       = g_modprint_exports;
-  modinfo->nexports      = 1;
-
-  return OK;
-}
