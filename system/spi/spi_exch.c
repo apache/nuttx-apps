9960297046ae6ed92d4bac3bba97a5e2b002fbae
zhangkai25 <zhangkai25@xiaomi.com>
2024-07-02 10:21:34 +0800

    Fixed the problem of memory trampling caused by not initializing the structure
    VELAPLATFO-36793
    
    Change-Id: Ice842c2204814bf92f81dd2c4923d059fe707b0b
    Signed-off-by: zhangkai25 <zhangkai25@xiaomi.com>

--- system/spi/spi_exch.c
+++ system/spi/spi_exch.c
@@ -169,6 +169,9 @@ int spicmd_exch(FAR struct spitool_s *spitool, int argc, FAR char **argv)
   trans.nwords = spitool->count;
   trans.txbuffer = txdata;
   trans.rxbuffer = rxdata;
+#ifdef CONFIG_SPI_HWFEATURES
+  trans.hwfeat = 0;
+#endif
 
   ret = spidev_transfer(fd, &seq);
 
