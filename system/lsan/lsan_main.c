c4f3f05bfa01c39eb1cc76fe22013c08e34dc7db
ligd <liguiding1@xiaomi.com>
2024-07-11 23:11:43 +0800

    lsan: add system cmd lsan support
    The Alsans support for the LeakSanitizer (lsan) memory leak detection tool.
    
    ref:
    https://github.com/gcc-mirror/gcc/blob/master/libsanitizer/include/sanitizer/lsan_interface.h
    
    This tool used for runtime check memleak on the SIM platfrom.
    
    Signed-off-by: ligd <liguiding1@xiaomi.com>

---
+++ system/lsan/lsan_main.c
@@ -0,0 +1,38 @@
+/****************************************************************************
+ * apps/system/lsan/lsan_main.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <stdio.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+extern void __lsan_do_recoverable_leak_check(void);
+
+int main(int argc, FAR char *argv[])
+{
+  __lsan_do_recoverable_leak_check();
+  return 0;
+}
