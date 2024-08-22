e1ad592c9dc68f911eb0eb3a8a57fc7f9520debf
Matheus Catarino <matheus-catarino@hotmail.com>
2024-08-22 14:15:29 -0300

    hello_swift added required: swift 6 (embedded module)

---
+++ examples/hello_swift/hello_swift_main.swift
@@ -0,0 +1,33 @@
+/****************************************************************************
+ * apps/examples/hello_swift/hello_swift_main.swift
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: hello_swift_main
+ ****************************************************************************/
+func swiftMain() {
+    print("Hello from Swift embedded!")
+}
+
+// Create a C-compatible entry point
+@_cdecl("hello_swift_main")
+public func cMain(_ argc: Int32, _ argv: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>) -> Int32 {
+    swiftMain()
+    return 0
+}
