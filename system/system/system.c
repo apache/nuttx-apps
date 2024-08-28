adef39ea1fa98a8ad2af7bfcfda7b6beb06d1d85
Bowen Wang <wangbowen6@xiaomi.com>
2024-08-28 16:39:44 +0800

    system/system: add config SYSTEM_SYSTEM_DUMPINFO
    In some cases, applicationes use system() to excute the nsh command,
    but it's hard to find out who used the system function to execute
    the nsh command, so  add this config to print the call backtrace and
    the command content.
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- system/system/system.c
+++ system/system/system.c
@@ -31,6 +31,8 @@
 #include <assert.h>
 #include <debug.h>
 #include <errno.h>
+#include <execinfo.h>
+#include <syslog.h>
 
 #include "nshlib/nshlib.h"
 
@@ -67,6 +69,11 @@ int system(FAR const char *cmd)
   int errcode;
   int rc;
   int ret;
+
+#ifdef CONFIG_SYSTEM_SYSTEM_DUMPINFO
+  syslog(LOG_INFO, "SYSTEM cmd=%s\n", cmd);
+  dump_stack();
+#endif
 
   /* REVISIT: If cmd is NULL, then system() should return a non-zero value to
    * indicate if the command processor is available or zero if it is not.
