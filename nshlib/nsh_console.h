6a2682e80de236ad911916a095c5a1ce1f481ef3
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-10-12 15:33:09 +0800

    nshlib: Fix stack-buffer-overflow of nsh_redirect()
    Board/Config/Command
      sim:nsh
      cat < /etc/init.d/rc.sysinit
    
    Problem
      The NSH hangs after exec the test command.
      `nsh_redirect()` needs to save three fd but array length is only two, stack buffer overflowed!
    
    Related: https://github.com/apache/nuttx-apps/pull/2469
    
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- nshlib/nsh_console.h
+++ nshlib/nsh_console.h
@@ -71,7 +71,7 @@
  * See struct serialsave_s in nsh_console.c
  */
 
-#define SAVE_SIZE (2 * sizeof(int))
+#define SAVE_SIZE (3 * sizeof(int))
 
 /* Are we using the NuttX console for I/O?  Or some other character device? */
 
