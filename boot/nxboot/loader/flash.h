61fec07c626399866a85802b243fd4db70aae87e
Michal Lenc <michallenc@seznam.cz>
2024-10-20 20:48:58 +0200

    boot: add NuttX bootloader with update and recovery support
    This commit adds NuttX based bootloader with the support for image
    update and recovery if not confirmed. The algorithm utilizes three
    flash partitions: primary (image runs from this area), secondary
    and tertiary. Secondary and tertiary areas are used for update upload
    and recovery.
    
    The update is performed by simple copy from update area to primary
    area with recovery being created in recovery area if not already present.
    Once image is confirmed by the user, the image in update area is
    confirmed as well, update area becomes recovery area and vice versa.
    This means the recovery is always present (except for the first update)
    and subsequent updates just copy image from update to primary. This
    makes the update significantly faster and more considerable to flash
    wear while keeping the recovery/revert possibility.
    
    A header (aligned to flash's erase size) must be added to the beginning
    of the image. Python script nximage.py can be used to prepend this
    header to built binary. The algorithm also uses one erase page at the
    end of a partition (partition, not image!) to store flags used to
    indicate image confirm status and to detect update/recovery partitions.
    Any program uploading update image to the update partition has to erase
    this page for the boot to work correctly!
    
    The algorithm implementation is based on a patch initially developed
    for MCUboot project but rejected by the project's maintainers
    
    https://github.com/mcu-tools/mcuboot/pull/1902
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

---
+++ boot/nxboot/loader/flash.h
@@ -0,0 +1,168 @@
+/****************************************************************************
+ * apps/boot/nxboot/loader/flash.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __BOOT_NXBOOT_LOADER_FLASH_H
+#define __BOOT_NXBOOT_LOADER_FLASH_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct flash_partition_info
+{
+  int size;
+  int blocksize;
+  int neraseblocks;
+};
+
+/****************************************************************************
+ * Public Functions Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: flash_partition_open
+ *
+ * Description:
+ *   Opens the partition based on a given name and returns the file
+ *   descriptor to it.
+ *
+ * Input parameters:
+ *   path: Path to the device.
+ *
+ * Returned Value:
+ *   Valid file descriptor on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_open(const char *path);
+
+/****************************************************************************
+ * Name: flash_partition_close
+ *
+ * Description:
+ *   Closes opened partition.
+ *
+ * Input parameters:
+ *   fd: Valid file descriptor.
+ *
+ * Returned Value:
+ *   0 on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_close(int fd);
+
+/****************************************************************************
+ * Name: flash_partition_write
+ *
+ * Description:
+ *   Writes count data pointed to by buf at offset off to a partition
+ *   referenced by file descriptor fd.
+ *
+ * Input parameters:
+ *   fd: Valid file descriptor.
+ *   buf: The pointer to data to be written.
+ *   count: Number of bytes to be written.
+ *   off: Write offset in bytes.
+ *
+ * Returned Value:
+ *   0 on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_write(int fd, const void *buf, size_t count, off_t off);
+
+/****************************************************************************
+ * Name: flash_partition_read
+ *
+ * Description:
+ *   Read count data to buffer buf at offset off from a partition
+ *   referenced by file descriptor fd.
+ *
+ * Input parameters:
+ *   fd: Valid file descriptor.
+ *   buf: The pointer where read data are stored.
+ *   count: Number of bytes to be read.
+ *   off: Read offset in bytes.
+ *
+ * Returned Value:
+ *   0 on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_read(int fd, void *buf, size_t count, off_t off);
+
+/****************************************************************************
+ * Name: flash_partition_erase
+ *
+ * Description:
+ *   Erases the entire partition.
+ *
+ * Input parameters:
+ *   fd: Valid file descriptor.
+ *
+ * Returned Value:
+ *   0 on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_erase(int fd);
+
+/****************************************************************************
+ * Name: flash_partition_erase_last_sector
+ *
+ * Description:
+ *   Erases the last sector of the partition
+ *
+ * Input parameters:
+ *   fd: Valid file descriptor.
+ *
+ * Returned Value:
+ *   0 on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_erase_last_sector(int fd);
+
+/****************************************************************************
+ * Name: flash_partition_info
+ *
+ * Description:
+ *   Returns the size of one block.
+ *
+ * Input parameters:
+ *   fd: Valid file descriptor.
+ *   info: Pointer to flash_partition_info structure where info is filled.
+ *
+ * Returned Value:
+ *   Size of the block on success, -1 on failure.
+ *
+ ****************************************************************************/
+
+int flash_partition_info(int fd, struct flash_partition_info *info);
+
+#endif /* __BOOT_NXBOOT_LOADER_FLASH_H */
---
+++ boot/nxboot/nxboot_main.c
@@ -0,0 +1,102 @@
+/****************************************************************************
+ * apps/boot/nxboot/nxboot_main.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <stdio.h>
+#include <syslog.h>
+
+#include <nxboot.h>
+#include <sys/boardctl.h>
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: nxboot_main
+ *
+ * Description:
+ *   NuttX bootlaoder entry point.
+ *
+ ****************************************************************************/
+
+int main(int argc, FAR char *argv[])
+{
+  struct boardioc_boot_info_s info;
+  bool check_only;
+#ifdef CONFIG_NXBOOT_SWRESET_ONLY
+  int ret;
+  FAR struct boardioc_reset_cause_s cause;
+#endif
+
+#if defined(CONFIG_BOARDCTL) && !defined(CONFIG_NSH_ARCHINIT)
+  /* Perform architecture-specific initialization (if configured) */
+
+  boardctl(BOARDIOC_INIT, 0);
+
+#ifdef CONFIG_BOARDCTL_FINALINIT
+  /* Perform architecture-specific final-initialization (if configured) */
+
+  boardctl(BOARDIOC_FINALINIT, 0);
+#endif
+#endif
+
+  syslog(LOG_INFO, "*** nxboot ***\n");
+
+#ifdef CONFIG_NXBOOT_SWRESET_ONLY
+  check_only = true;
+  ret = boardctl(BOARDIOC_RESET_CAUSE, (uintptr_t)&cause);
+  if (ret >= 0)
+    {
+      if (cause.cause == BOARDIOC_RESETCAUSE_CPU_SOFT ||
+          cause.cause == BOARDIOC_RESETCAUSE_CPU_RWDT ||
+          cause.cause == BOARDIOC_RESETCAUSE_PIN)
+        {
+          check_only = false;
+        }
+      else
+        {
+          syslog(LOG_INFO, "Power reset detected, performing check only.\n");
+        }
+    }
+#else
+  check_only = false;
+#endif
+
+  if (nxboot_perform_update(check_only) < 0)
+    {
+      syslog(LOG_ERR, "Could not find bootable image.\n");
+      return 0;
+    }
+
+  syslog(LOG_INFO, "Found bootable image, boot from primary.\n");
+
+  /* Call board specific image boot */
+
+  info.path        = CONFIG_NXBOOT_PRIMARY_SLOT_PATH;
+  info.header_size = CONFIG_NXBOOT_HEADER_SIZE;
+
+  return boardctl(BOARDIOC_BOOT_IMAGE, (uintptr_t)&info);
+}
