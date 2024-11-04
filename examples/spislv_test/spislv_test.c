a4698c31311560aa12eec9230efcbb0764135c71
Felipe Moura <moura.fmo@gmail.com>
2024-11-04 14:49:34 -0300

    examples:spislave_test: Improve example

--- examples/spislv_test/spislv_test.c
+++ examples/spislv_test/spislv_test.c
@@ -31,7 +31,8 @@
 #include <stdlib.h>
 
 #define SOURCE_FILE "/dev/spislv2"
-#define BUFFER_SIZE 256
+#define RX_BUFFER_SIZE 32
+#define TX_BUFFER_SIZE 16
 
 /****************************************************************************
  * Public Functions
@@ -44,59 +45,67 @@
 int main(int argc, FAR char *argv[])
 {
   int fd;
-  char buffer[BUFFER_SIZE];
+  char buffer_rx[RX_BUFFER_SIZE];
+  char buffer_tx[TX_BUFFER_SIZE] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,0x9, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
   ssize_t bytes_read;
+  ssize_t bytes_written;
   ssize_t i;
+  size_t current_tx_index = 0; // Índice para escrita circular
 
   printf("Slave started!!\n");
   fd = open(SOURCE_FILE, O_RDWR);
 
   if (fd < 0)
     {
-      printf("Failed to open %s: %s\n", SOURCE_FILE, strerror(errno));
+      printf("Failed to open %s\n", SOURCE_FILE);
       return 0;
     }
 
+  printf("Slave: Reading from %s\n", SOURCE_FILE);
+
   while (1)
+  {
+    /* Read the number from the source file */
+
+    bytes_read = read(fd, buffer_rx, RX_BUFFER_SIZE - 1);
+
+    if (bytes_read < 0)
+      {
+        printf("Failed to read from %s\n", SOURCE_FILE);
+        close(fd);
+        return 0;
+      }
+    else if (bytes_read > 0)
     {
-      /* Read the number from the source file */
+      ssize_t bytes_to_write = 2U;
+      ssize_t total_written = 0;
 
-      printf("Slave: Reading from %s\n", SOURCE_FILE);
-      bytes_read = read(fd, buffer, BUFFER_SIZE - 1);
+      while (bytes_to_write > 0)
+      {
+        size_t bytes_available = TX_BUFFER_SIZE - current_tx_index;
+        size_t chunk_size = (bytes_to_write < bytes_available) ? bytes_to_write : bytes_available;
 
-      if (bytes_read < 0)
+        // Escreve o chunk_size de buffer_tx a partir de current_tx_index
+        bytes_written = write(fd, &buffer_tx[current_tx_index], chunk_size);
+        
+        printf("Queued for sending to master: ");
+        for (i = 0; i < chunk_size; i++)
         {
-          printf("Failed to read from %s: %s\n",
-                    SOURCE_FILE, strerror(errno));
-          close(fd);
-          return 0;
+          printf("%02x ", buffer_tx[(current_tx_index + i) % TX_BUFFER_SIZE]);
         }
-      else if (bytes_read > 0)
+        printf("\n");
+
+        if (bytes_written < 0)
         {
-          buffer[bytes_read] = '\0';
-
-          /* Print buffer in hexadecimal format */
-
-          printf("Slave: Read value in hex: ");
-          for (i = 0; i < bytes_read; ++i)
-            {
-              printf("%02x ", (unsigned char)buffer[i]);
-            }
-
-          printf("\n");
-
-          /* Write the same value back */
-
-          printf("Slave: Writing %d bytes back to %s\n",
-                    bytes_read, SOURCE_FILE);
-          ssize_t bytes_written = write(fd, buffer, bytes_read);
-          if (bytes_written < 0)
-            {
-              printf("Failed to write to %s: %s\n",
-                        SOURCE_FILE, strerror(errno));
-              close(fd);
-              return 0;
-            }
+            printf("Falha ao escrever para %s: %s\n", SOURCE_FILE, strerror(errno));
+            close(fd);
+            return 0;
         }
+
+        total_written += bytes_written;
+        bytes_to_write -= bytes_written;
+        current_tx_index = (current_tx_index + bytes_written) % TX_BUFFER_SIZE;
+      }
     }
+  }
 }
