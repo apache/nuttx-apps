4cdbc96d3e9b444d330827c60a3cde43971ff877
George Poulios <gpoulios@census-labs.com>
2024-12-01 02:11:17 +0200

    Update MbedTLS 3.4.0 -> 3.6.2 and set it as default
    Even though one could in theory simply set CONFIG_MBEDTLS_VERSION="3.6.2",
    this commit is needed for 2 reasons:
      1. The patches need to be updated: 0001 works almost as-is (with fuzz 2)
         but 0002 hunk #4 fails and needed adjustment. Otherwise, the patches
         are identical.
      2. mbedtls_config.h needs to be updated: Following the same approach
         for the Kconfig this commit updates both Kconfig and mbedtls_config.h
         according to the changes:
           mbedtls$ git diff v3.4.0...v3.6.2 -- include/mbedtls/mbedtls_config.h
    
    Minor edits also to crypto/controlse to comply with v3.6.2.
    
    Signed-off-by: George Poulios <gpoulios@census-labs.com>

--- crypto/controlse/cmbedtls_se05x_extension.hxx
+++ crypto/controlse/cmbedtls_se05x_extension.hxx
@@ -89,7 +89,7 @@ public:
     return result;
   }
 
-  static int ecdsa_sign_wrap(void *ctx, mbedtls_md_type_t md_alg,
+  static int ecdsa_sign_wrap(mbedtls_pk_context *pk, mbedtls_md_type_t md_alg,
                              const unsigned char *hash, size_t hash_len,
                              unsigned char *sig, size_t sig_size,
                              size_t *sig_len,
@@ -97,7 +97,8 @@ public:
                              void *p_rng)
   {
 
-    mbedtls_ecp_keypair *key = reinterpret_cast<mbedtls_ecp_keypair *>(ctx);
+    mbedtls_ecp_keypair *key =
+        reinterpret_cast<mbedtls_ecp_keypair *>(pk->pk_ctx);
     auto se05x_ctx = reinterpret_cast<mbedtls_se05x_ctx *>(key->d.p);
 
     struct se05x_signature_s args
