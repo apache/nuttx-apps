7e9cf0237dcd31ba48d116e5b053fae25dfe7f99
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-17 07:27:52 +0100

    audioutils: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- audioutils/fmsynth/test/fmsynth_op_test.c
+++ audioutils/fmsynth/test/fmsynth_op_test.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * apps/audioutils/fmsynth/test/fmsynth_op_test.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
