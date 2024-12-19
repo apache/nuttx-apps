e785065c2e4f246737bc63db8d7fe0931884bc84
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-19 04:23:30 +0100

    graphics: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- graphics/nxwidgets/src/clabelgrid.cxx
+++ graphics/nxwidgets/src/clabelgrid.cxx
@@ -1,6 +1,8 @@
 /****************************************************************************
  * apps/graphics/NxWkidgets/nwidgets/src/clabelgrid.cxx
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
