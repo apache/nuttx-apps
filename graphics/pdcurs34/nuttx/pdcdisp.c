ed1a8c6d957f13a4b5b3598755cb8f62e3e87af9
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-19 04:23:30 +0100

    graphics: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- graphics/pdcurs34/nuttx/pdcdisp.c
+++ graphics/pdcurs34/nuttx/pdcdisp.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * apps/graphics/pdcurs34/nuttx/pdcdisp.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
