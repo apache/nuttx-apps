064c6e16f776cee6b355c5f151f54f4f78e34d97
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-19 04:54:28 +0100

    include: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- include/graphics/twm4nx/twm4nx_cursor.hxx
+++ include/graphics/twm4nx/twm4nx_cursor.hxx
@@ -1,6 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////
 // apps/include/graphics/twm4nx/twm4nx_cursor.hxx
 //
+// SPDX-License-Identifier: Apache-2.0
+//
 // Licensed to the Apache Software Foundation (ASF) under one or more
 // contributor license agreements.  See the NOTICE file distributed with
 // this work for additional information regarding copyright ownership.  The
--- include/netutils/httpd.h
+++ include/netutils/httpd.h
@@ -1,15 +1,11 @@
 /****************************************************************************
  * apps/include/netutils/httpd.h
  *
- *   Copyright (C) 2007, 2009, 2011-2012, 2014, 2018 Gregory Nutt. All
- *     rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
- *
- * Based on uIP which also has a BSD style license:
- *
- *   Author: Adam Dunkels <adam@sics.se>
- *   Copyright (c) 2001-2005, Adam Dunkels.
- *   All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: 2007, 2009-2011, 2014, 2018 Gregory Nutt
+ * SPDX-FileCopyrightText: 2001-2005, Adam Dunkels.
+ * SPDX-FileContributor: Gregory Nutt <gnutt@nuttx.org>
+ * SPDX-FileContributor: Adam Dunkels <adam@sics.se>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
