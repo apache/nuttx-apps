2a89ee6cb73fcdcf7f8c03e24f1caa9dd09b8115
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-20 04:22:12 +0100

    lte: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- lte/alt1250/usock_handlers/alt1250_bindhdlr.c
+++ lte/alt1250/usock_handlers/alt1250_bindhdlr.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * apps/lte/alt1250/usock_handlers/alt1250_bindhdlr.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
--- lte/alt1250/usock_handlers/alt1250_ioctl_subhdlr.h
+++ lte/alt1250/usock_handlers/alt1250_ioctl_subhdlr.h
@@ -1,6 +1,8 @@
 /****************************************************************************
  * apps/lte/alt1250/usock_handlers/alt1250_ioctl_subhdlr.h
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
