1e4f1fd303ffae1fdff25fdb882205a76a34fd78
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-20 05:58:52 +0100

    modbus: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- modbus/mb_m.c
+++ modbus/mb_m.c
@@ -1,8 +1,8 @@
 /****************************************************************************
  * FreeModbus Library: A portable Modbus implementation for Modbus ASCII/RTU.
  *
- *   Copyright (C) 2013 Armink <armink.ztl@gmail.com>
- *   All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: 2013 Armink <armink.ztl@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
