092c977136e269832e31e7fd13a7cc344b333129
tengshuangshuang <tengshuangshuang@xiaomi.com>
2024-12-26 17:26:55 +0800

    tests:Merge the same test cases under testing/testsuites into the testing folder
    Signed-off-by: tengshuangshuang <tengshuangshuang@xiaomi.com>

---
+++ testing/socket/cm_socket_test/cases/net_socket_test_010.c
@@ -0,0 +1,259 @@
+/****************************************************************************
+ * apps/testing/testsuites/kernel/socket/cases/net_socket_test_010.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ *The ASF licenses this file to you under the Apache License, Version 2.0
+ *(the "License"); you may not use this file except in compliance with
+ *the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *implied.  See the License for the specific language governing
+ *permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+#include <nuttx/config.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <inttypes.h>
+#include <fcntl.h>
+
+#include "SocketTest.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define IFF_PROMISC 0x100
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+static char gdefaultnetif[IFNAMSIZ] = "eth0";
+
+static void initifreq(struct ifreq *ifr)
+{
+  *ifr = (struct ifreq)
+    {
+        {
+          0
+        }
+    };
+
+  /* (void)strncpy_s(ifr->ifr_name, sizeof(ifr->ifr_name) - 1, */
+
+  /* gdefaultnetif, sizeof(ifr->ifr_name) - 1); */
+
+  (void)strlcpy(ifr->ifr_name, gdefaultnetif, sizeof(ifr->ifr_name));
+  ifr->ifr_name[sizeof(ifr->ifr_name) - 1] = '\0';
+}
+
+static char *ifindex2name(int fd, unsigned index, char *name)
+{
+#ifdef CONFIG_NETDEV_IFINDEX
+  return if_indextoname(index, name);
+#else
+  struct ifreq ifr;
+  int ret;
+
+  ifr.ifr_ifindex = index;
+  ret = ioctl(fd, SIOCGIFNAME, &ifr);
+  if (ret < 0)
+    {
+      return NULL;
+    }
+
+  /* ret = strncpy_s(name, IF_NAMESIZE - 1, ifr.ifr_name, IF_NAMESIZE -
+   * 1);
+   */
+
+  strncpy(name, ifr.ifr_name, IF_NAMESIZE - 1);
+  name[IF_NAMESIZE - 1] = '\0';
+  return name;
+#endif
+}
+
+static unsigned ifname2index(int fd, const char *name)
+{
+#ifdef CONFIG_NETDEV_IFINDEX
+  return if_nametoindex(name);
+#else
+  struct ifreq ifr;
+  int ret;
+
+  /* (void)strncpy_s(ifr.ifr_name, sizeof(ifr.ifr_name) - 1, name,
+   * sizeof(ifr.ifr_name) - 1);
+   */
+
+  (void)strncpy(ifr.ifr_name, name, sizeof(ifr.ifr_name) - 1);
+  ifr.ifr_name[sizeof(ifr.ifr_name) - 1] = '\0';
+  ret = ioctl(fd, SIOCGIFINDEX, &ifr);
+  return ret < 0 ? 0 : ifr.ifr_ifindex;
+#endif
+}
+
+/****************************************************************************
+ * Name: test_nuttx_net_socket_test10
+ ****************************************************************************/
+
+static int ioctltestinternal(int sfd)
+{
+  struct ifreq ifr =
+  {
+    {
+      0
+    }
+  };
+
+  char ifname[IFNAMSIZ] =
+  {
+    0
+  };
+
+  char *p = NULL;
+  unsigned int loindex = 0;
+  unsigned int lanindex = 0;
+  char lanname[IFNAMSIZ];
+  int maxindex = 256;
+  int ret;
+  char *ret_s = NULL;
+
+  for (int i = 0; i < maxindex; ++i)
+    {
+      p = ifindex2name(sfd, i, ifname);
+      if (p)
+        {
+          if (strcmp(p, "lo") == 0)
+            {
+              loindex = i;
+            }
+
+          else
+            {
+              lanindex = i;
+              strcpy(lanname, p);
+              syslog(LOG_INFO, "name of lan:  %s", lanname);
+            }
+        }
+    }
+
+  syslog(LOG_INFO, "ifindex of lo: %u, ifindex of lan: %u", loindex,
+         lanindex);
+  assert_int_not_equal(loindex, 0);
+  assert_int_not_equal(lanindex, 0);
+
+  p = ifindex2name(sfd, loindex, ifname);
+  syslog(LOG_INFO, "ifindex %u: %s", loindex, p);
+  assert_non_null(p);
+  assert_string_equal(p, "lo");
+
+  p = ifindex2name(sfd, lanindex, ifname);
+  syslog(LOG_INFO, "ifindex %u: %s", lanindex, p);
+  assert_non_null(p);
+  assert_string_equal(p, lanname);
+
+  /* ret = strncpy_s(gdefaultnetif, sizeof(gdefaultnetif) -1, p,
+   * sizeof(gdefaultnetif) - 1);
+   */
+
+  ret_s = strncpy(gdefaultnetif, p, sizeof(gdefaultnetif) - 1);
+  assert_non_null(ret_s);
+  gdefaultnetif[sizeof(gdefaultnetif) - 1] = '\0';
+
+  ret = (int)ifname2index(sfd, p);
+  syslog(LOG_INFO, "index of %s: %d", p, ret);
+  assert_int_not_equal(ret, 0);
+  assert_int_equal(ret, lanindex);
+
+  ifr.ifr_ifindex = lanindex;
+  ret = ioctl(sfd, SIOCGIFNAME, &ifr);
+  assert_int_equal(ret, 0);
+  syslog(LOG_INFO, "name of ifindex %u: %s", lanindex, ifr.ifr_name);
+  assert_string_equal(ifr.ifr_name, lanname);
+
+  initifreq(&ifr);
+  ret = ioctl(sfd, SIOCGIFINDEX, &ifr);
+  assert_int_equal(ret, 0);
+  syslog(LOG_INFO, "index of ifname %s: %d", ifr.ifr_name,
+         ifr.ifr_ifindex);
+  assert_int_equal(ifr.ifr_ifindex, lanindex);
+
+  initifreq(&ifr);
+  ret = ioctl(sfd, SIOCGIFHWADDR, &ifr);
+  assert_int_equal(ret, 0);
+  syslog(LOG_INFO, "hwaddr: %02hhX:%02hhX:%02hhX:XX:XX:XX",
+         ifr.ifr_hwaddr.sa_data[0], ifr.ifr_hwaddr.sa_data[1],
+         ifr.ifr_hwaddr.sa_data[2]);
+
+  initifreq(&ifr);
+  ret = ioctl(sfd, SIOCGIFFLAGS, &ifr);
+  assert_int_equal(ret, 0);
+  syslog(LOG_INFO, "FLAGS of ifname %s: %#" PRIx32 ", IFF_PROMISC: %d",
+         ifr.ifr_name, ifr.ifr_flags, !!(ifr.ifr_flags & IFF_PROMISC));
+
+  if (ifr.ifr_flags & IFF_PROMISC)
+    {
+      ifr.ifr_flags &= ~(IFF_PROMISC);
+    }
+
+  else
+    {
+      ifr.ifr_flags |= IFF_PROMISC;
+    }
+
+  syslog(LOG_INFO, "SIOCSIFFLAGS FLAGS: %#" PRIx32, ifr.ifr_flags);
+  ret = ioctl(sfd, SIOCSIFFLAGS, &ifr);
+  if (ret == -1)
+    {
+      assert_int_equal(errno, EPERM);
+    }
+
+  else
+    {
+      assert_int_equal(ret, 0);
+    }
+
+  initifreq(&ifr);
+  ret = ioctl(sfd, SIOCGIFFLAGS, &ifr);
+  assert_int_equal(ret, 0);
+  syslog(LOG_INFO, "FLAGS of ifname %s: %#" PRIx32 ", IFF_PROMISC: %d",
+         ifr.ifr_name, ifr.ifr_flags, !!(ifr.ifr_flags & IFF_PROMISC));
+
+  ret = fcntl(sfd, F_GETFL, 0);
+  assert_int_equal(ret < 0, 0);
+
+  ret = fcntl(sfd, F_SETFL, ret | O_NONBLOCK);
+  assert_int_equal(ret < 0, 0);
+
+  return 0;
+}
+
+void test_nuttx_net_socket_test10(FAR void **state)
+{
+  int sfd;
+
+  sfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  syslog(LOG_INFO, "socket(PF_INET, SOCK_STREAM, IPPROTO_TCP): %d", sfd);
+  assert_int_not_equal(sfd, -1);
+
+  (void)ioctltestinternal(sfd);
+
+  (void)close(sfd);
+}
---
+++ testing/syscall/cm_syscall_test/include/SyscallTest.h
@@ -0,0 +1,457 @@
+/****************************************************************************
+ * apps/testing/testsuites/kernel/syscall/include/SyscallTest.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ *The ASF licenses this file to you under the Apache License, Version 2.0
+ *(the "License"); you may not use this file except in compliance with
+ *the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *implied.  See the License for the specific language governing
+ *permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __SYSCALLTEST_H
+#define __SYSCALLTEST_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+#include <nuttx/config.h>
+#include <dirent.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <dirent.h>
+
+/* Functions to help test */
+
+int checknames(char *pfilnames, DIR *ddir);
+int check_and_report_ftruncatetest(int fd, off_t offset, char data,
+                                   off_t trunc_len);
+int tst_fill_file(const char *path, char pattern, size_t bs,
+                  size_t bcount);
+
+char *sock_addr(const struct sockaddr *sa, socklen_t salen, char *res,
+                size_t len);
+
+/* @return port in network byte order.
+ */
+
+unsigned short get_unused_port(void(cleanup_fn)(void),
+                               unsigned short family, int type);
+
+int safe_close(int fildes);
+
+int safe_socket(int domain, int type, int protocol);
+
+int safe_connect(int sockfd, const struct sockaddr *addr,
+                 socklen_t addrlen);
+
+int safe_getsockname(int sockfd, struct sockaddr *addr,
+                     socklen_t *addrlen);
+
+int safe_bind(int socket, const struct sockaddr *address,
+              socklen_t address_len);
+
+int safe_listen(int socket, int backlog);
+
+int safe_open(const char *pathname, int oflags, ...);
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* The test files generated during the 'syscall-test' are stored in this
+ * directory
+ */
+
+#define SYSCALL_TEST_DIR "syscall_test_dir"
+
+#define MOUNT_DIR CONFIG_TESTS_SYSCALL_MOUNT_DIR
+
+#define SAFE_OPEN(pathname, oflags, ...)                                \
+  safe_open((pathname), (oflags), ##__VA_ARGS__)
+
+#define SAFE_FCNTL(fd, cmd, ...)                                        \
+  ({                                                                    \
+    int tst_ret_ = fcntl(fd, cmd, ##__VA_ARGS__);                       \
+    if (tst_ret_ == -1)                                                 \
+      {                                                                 \
+        syslog(LOG_ERR, "fcntl(%i,%s,...) failed", fd, #cmd);           \
+        fail_msg("test fail !");                                        \
+      }                                                                 \
+    tst_ret_ == -1 ? 0 : tst_ret_;                                      \
+  })
+
+#define SAFE_CLOSE(fd)                                                  \
+  do                                                                    \
+    {                                                                   \
+      safe_close((fd));                                                 \
+      fd = -1;                                                          \
+  } while (0)
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/* quickly fill a test file */
+
+int cmtestfillfile(const char *path, char pattern, size_t bs,
+                   size_t bcount);
+
+/* setup function */
+
+int test_nuttx_syscall_test_group_setup(void **state);
+
+/* teardown function */
+
+int test_nuttx_syscall_test_group_teardown(void **state);
+
+/* tools */
+
+int checknames(char *pfilnames, DIR *ddir);
+int check_and_report_ftruncatetest(int fd, off_t offset, char data,
+                                   off_t trunc_len);
+int tst_fill_fd(int fd, char pattern, size_t bs, size_t bcount);
+int tst_fill_file(const char *path, char pattern, size_t bs,
+                  size_t bcount);
+int safe_open(const char *pathname, int oflags, ...);
+ssize_t safe_write(int fildes, const void *buf, size_t nbyte);
+off_t safe_lseek(int fd, off_t offset, int whence);
+int safe_close(int fildes);
+void safe_touch(const char *file, const int lineno, const char *pathname,
+                mode_t mode);
+#ifdef CONFIG_NET
+char *sock_addr(const struct sockaddr *sa, socklen_t salen, char *res,
+                size_t len);
+unsigned short get_unused_port(void(cleanup_fn)(void),
+                               unsigned short family, int type);
+int safe_socket(int domain, int type, int protocol);
+int safe_connect(int sockfd, const struct sockaddr *addr,
+                 socklen_t addrlen);
+int safe_getsockname(int sockfd, struct sockaddr *addr,
+                     socklen_t *addrlen);
+int safe_bind(int socket, const struct sockaddr *address,
+              socklen_t address_len);
+int safe_listen(int socket, int backlog);
+#endif
+
+/* test case function */
+
+/* cases/chdir_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_chdir01(FAR void **state);
+void test_nuttx_syscall_chdir02(FAR void **state);
+
+/* cases/accept_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_accept01(FAR void **state);
+
+/* cases/getitimer_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_getitimer01(FAR void **state);
+
+/* cases/clock_gettime_test.c
+ * ****************************************/
+
+void test_nuttx_syscall_clockgettime01(FAR void **state);
+
+/* cases/clock_nanosleep_test.c
+ * **************************************/
+
+void test_nuttx_syscall_clocknanosleep01(FAR void **state);
+void test_nuttx_syscall_clocknanosleep02(FAR void **state);
+
+/* cases/clock_settime_test.c
+ * ****************************************/
+
+void test_nuttx_syscall_clocksettime01(FAR void **state);
+
+/* cases/close_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_close01(FAR void **state);
+void test_nuttx_syscall_close02(FAR void **state);
+void test_nuttx_syscall_close03(FAR void **state);
+
+/* cases/creat_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_creat01(FAR void **state);
+void test_nuttx_syscall_creat02(FAR void **state);
+
+/* cases/fcntl_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_fcntl01(FAR void **state);
+void test_nuttx_syscall_fcntl02(FAR void **state);
+void test_nuttx_syscall_fcntl03(FAR void **state);
+void test_nuttx_syscall_fcntl04(FAR void **state);
+void test_nuttx_syscall_fcntl05(FAR void **state);
+void test_nuttx_syscall_fcntl06(FAR void **state);
+
+/* cases/fsatfs_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_fstatfs01(FAR void **state);
+
+/* cases/fsync_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_fsync01(FAR void **state);
+void test_nuttx_syscall_fsync02(FAR void **state);
+void test_nuttx_syscall_fsync03(FAR void **state);
+
+/* cases/ftruncate_test.c
+ * ********************************************/
+
+void test_nuttx_syscall_ftruncate01(FAR void **state);
+
+/* cases/getcwd_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_getcwd01(FAR void **state);
+void test_nuttx_syscall_getcwd02(FAR void **state);
+
+/* cases/getpid_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_getpid01(FAR void **state);
+
+/* cases/getppid_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_getppid01(FAR void **state);
+
+/* cases/gethostname_test.c
+ * ******************************************/
+
+void test_nuttx_syscall_gethostname01(FAR void **state);
+
+/* cases/getTimeofday_test.c
+ * *****************************************/
+
+void test_nuttx_syscall_gettimeofday01(FAR void **state);
+
+/* cases/lseek_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_lseek01(FAR void **state);
+void test_nuttx_syscall_lseek07(FAR void **state);
+
+/* cases/lstat_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_lstat01(FAR void **state);
+
+/* cases/dup_test.c
+ * **************************************************/
+
+void test_nuttx_syscall_dup01(FAR void **state);
+void test_nuttx_syscall_dup02(FAR void **state);
+void test_nuttx_syscall_dup03(FAR void **state);
+void test_nuttx_syscall_dup04(FAR void **state);
+void test_nuttx_syscall_dup05(FAR void **state);
+
+/* cases/dup2_test.c
+ * **************************************************/
+
+void test_nuttx_syscall_dup201(FAR void **state);
+void test_nuttx_syscall_dup202(FAR void **state);
+
+/* cases/fpathconf_test.c
+ * *********************************************/
+
+void test_nuttx_syscall_fpathconf01(FAR void **state);
+
+/* cases/getegid_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_getegid01(FAR void **state);
+void test_nuttx_syscall_getegid02(FAR void **state);
+
+/* cases/geteuid_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_geteuid01(FAR void **state);
+void test_nuttx_syscall_geteuid02(FAR void **state);
+
+/* cases/getgid_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_getgid01(FAR void **state);
+void test_nuttx_syscall_getgid02(FAR void **state);
+
+/* cases/getuid_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_getuid01(FAR void **state);
+void test_nuttx_syscall_getuid02(FAR void **state);
+
+/* cases/pathconf_test.c
+ * *********************************************/
+
+void test_nuttx_syscall_pathconf01(FAR void **state);
+
+/* cases/pipe_test.c
+ * *************************************************/
+
+void test_nuttx_syscall_pipe01(FAR void **state);
+void test_nuttx_syscall_pipe02(FAR void **state);
+
+/* cases/pread_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_pread01(FAR void **state);
+
+/* cases/pwrite_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_pwrite01(FAR void **state);
+void test_nuttx_syscall_pwrite02(FAR void **state);
+
+/* cases/rmdir_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_rmdir01(FAR void **state);
+void test_nuttx_syscall_rmdir02(FAR void **state);
+
+/* ases/syscall_truncate_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_truncate01(FAR void **state);
+
+/* cases/unlink_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_unlink01(FAR void **state);
+
+/* cases/nansleep_test.c
+ * *********************************************/
+
+void test_nuttx_syscall_nansleep01(FAR void **state);
+void test_nuttx_syscall_nansleep02(FAR void **state);
+
+/* cases/time_test.c
+ * *************************************************/
+
+void test_nuttx_syscall_time01(FAR void **state);
+void test_nuttx_syscall_time02(FAR void **state);
+
+/* cases/timer_create_test.c
+ * *****************************************/
+
+void test_nuttx_syscall_timercreate01(FAR void **state);
+
+/* cases/timer_delete_test.c
+ * *****************************************/
+
+void test_nuttx_syscall_timerdelete01(FAR void **state);
+
+/* cases/timer_gettime_test.c
+ * ****************************************/
+
+void test_nuttx_syscall_timergettime01(FAR void **state);
+
+/* cases/mkdir_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_mkdir01(FAR void **state);
+void test_nuttx_syscall_mkdir02(FAR void **state);
+void test_nuttx_syscall_mkdir03(FAR void **state);
+
+/*  cases/syscall_sched_test.c
+ * ***********************************************/
+
+void test_nuttx_syscall_sched01(FAR void **state);
+void test_nuttx_syscall_sched02(FAR void **state);
+void test_nuttx_syscall_sched03(FAR void **state);
+void test_nuttx_syscall_sched04(FAR void **state);
+
+/* cases/write_test.c
+ * ************************************************/
+
+void test_nuttx_syscall_write01(FAR void **state);
+void test_nuttx_syscall_write02(FAR void **state);
+void test_nuttx_syscall_write03(FAR void **state);
+
+/* cases/read_test.c
+ * *************************************************/
+
+void test_nuttx_syscall_read01(FAR void **state);
+void test_nuttx_syscall_read02(FAR void **state);
+void test_nuttx_syscall_read03(FAR void **state);
+void test_nuttx_syscall_read04(FAR void **state);
+
+/* cases/symlink_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_symlink01(FAR void **state);
+void test_nuttx_syscall_symlink02(FAR void **state);
+
+/* cases/socket_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_sockettest01(FAR void **state);
+void test_nuttx_syscall_sockettest02(FAR void **state);
+
+/* cases/getpeername_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_connect01(FAR void **state);
+
+/* cases/getpeername_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_getpeername01(FAR void **state);
+
+/* cases/getsocketopt_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_getsockopt01(FAR void **state);
+
+/* cases/recvfrom_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_recvfromtest01(FAR void **state);
+
+/* cases/setsocketopt01_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_setsockopt01(FAR void **state);
+
+/* cases/listen_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_listen01(FAR void **state);
+
+/* cases/socketpair_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_socketpair01(FAR void **state);
+void test_nuttx_syscall_socketpair02(FAR void **state);
+
+/* cases/bind_test.c
+ * **********************************************/
+
+void test_nuttx_syscall_bind01(FAR void **state);
+
+#endif
