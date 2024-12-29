812ad8d99a2cc16d1cf90cb82f048cca4bb0b0d5
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-29 12:41:39 +0100

    tools: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- tools/Wasm.mk
+++ tools/Wasm.mk
@@ -1,6 +1,7 @@
-
 ############################################################################
 # apps/tools/Wasm.mk
+#
+# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
