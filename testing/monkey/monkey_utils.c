068220d3a7b6db0f93e20a4f5a7c52c2b69c5779
Alin Jerpelea <alin.jerpelea@sony.com>
2024-12-29 16:26:09 +0100

    testing: migrate to SPDX identifier
    Most tools used for compliance and SBOM generation use SPDX identifiers
    This change brings us a step closer to an easy SBOM generation.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- testing/monkey/monkey_utils.c
+++ testing/monkey/monkey_utils.c
@@ -1,6 +1,8 @@
 /****************************************************************************
  * apps/testing/monkey/monkey_utils.c
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.  The
--- testing/nand_sim/nand_sim_main.c
+++ testing/nand_sim/nand_sim_main.c
@@ -1,6 +1,7 @@
 /****************************************************************************
  * apps/testing/nand_sim/nand_sim_main.c
  *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
