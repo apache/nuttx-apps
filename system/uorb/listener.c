cbab1dcb5aa0d35d60c09bff72b261be544cfb72
likun17 <likun17@xiaomi.com>
2025-01-13 20:32:00 +0800

    system/uorb: uorb listener bug fix.
    Fixed the issue that instance+1 subscription will be triggered when
    topic exists in this core
    
    Signed-off-by: likun17 <likun17@xiaomi.com>

--- system/uorb/listener.c
+++ system/uorb/listener.c
@@ -427,7 +427,7 @@ static int listener_generate_object_list(FAR struct listen_list_s *objlist,
                         }
                     }
 
-                  if (orb_exists(object.meta, object.instance++) < 0)
+                  if (orb_exists(object.meta, ++object.instance) < 0)
                     {
                       break;
                     }
