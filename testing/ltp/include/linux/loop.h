e6423d04850ea3db1c0694823221b719e046296d
guoshichao <guoshichao@xiaomi.com>
2025-01-24 10:44:10 +0800

    testing/ltp: add linux struct loop_info struct definition
    Define the complete loop_info structure and LOOP_* ioctl command constants
    in testing/ltp/include/linux/loop.h, matching Linux kernel's loop device
    interface. This allows LTP loop device testcases to build and execute
    when NuttX loop device support (CONFIG_DEV_LOOP) is available.
    
    Signed-off-by: guoshichao <guoshichao@xiaomi.com>

--- testing/ltp/include/linux/loop.h
+++ testing/ltp/include/linux/loop.h
@@ -20,8 +20,8 @@
  *
  ****************************************************************************/
 
-#ifndef _LINUX_LOOP_H
-#define _LINUX_LOOP_H
+#ifndef _LTP_LINUX_LOOP_H
+#define _LTP_LINUX_LOOP_H
 
 /****************************************************************************
  * Included Files
@@ -29,4 +29,46 @@
 
 #include <nuttx/fs/loop.h>
 
-#endif /* _LINUX_LOOP_H */
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#ifdef CONFIG_DEV_LOOP
+
+#define LOOP_SET_FD       0x4C00
+#define LOOP_CLR_FD       0x4C01
+#define LOOP_SET_STATUS   0x4C02
+#define LOOP_GET_STATUS   0x4C03
+#define LOOP_SET_STATUS64 0x4C04
+#define LOOP_GET_STATUS64 0x4C05
+#define LOOP_CHANGE_FD    0x4C06
+
+#define LO_NAME_SIZE      64
+#define LO_KEY_SIZE       32
+
+#endif
+
+/****************************************************************************
+ * Type Definitions
+ ****************************************************************************/
+
+#ifdef CONFIG_DEV_LOOP
+
+struct loop_info
+{
+  int                lo_number;                   /* ioctl r/o */
+  uint16_t           lo_device;                   /* ioctl r/o */
+  unsigned long      lo_inode;                    /* ioctl r/o */
+  uint16_t           lo_rdevice;                  /* ioctl r/o */
+  int                lo_offset;
+  int                lo_encrypt_key_size;         /* ioctl w/o */
+  int                lo_flags;
+  char               lo_name[LO_NAME_SIZE];
+  unsigned char      lo_encrypt_key[LO_KEY_SIZE]; /* ioctl w/o */
+  unsigned long      lo_init[2];
+  char               reserved[4];
+};
+
+#endif
+
+#endif /* _LTP_LINUX_LOOP_H */
