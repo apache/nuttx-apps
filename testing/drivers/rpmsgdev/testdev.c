ca2032941da9e2158e63b264a3ae51718e019fcd
Xiang Xiao <xiaoxiang@xiaomi.com>
2025-01-26 16:05:36 +0800

    testing: Move rpmsgdev to drivers/rpmsgdev
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- testing/drivers/rpmsgdev/testdev.c
+++ testing/drivers/rpmsgdev/testdev.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * apps/testing/rpmsgdev/testdev.c
+ * apps/testing/drivers/rpmsgdev/testdev.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
