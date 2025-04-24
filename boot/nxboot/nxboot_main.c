b7f5fda9eb2065c7f1c051be546940ae55cc39b9
Tim Hardisty <56726697+TimJTi@users.noreply.github.com>
2025-04-24 11:15:26 +0100

    apps/boot/nxboot: Enhancements to add progress messages and copy-to-RAM feature
    This adds Kconfig-enabled progress messages that are output to stdout.
    
    It also adds Kconfig-enabled option to copy the validated and bootable image to RAM
    
    Signed-off by: Tim Hardisty <timh@jti.uk.com>

--- boot/nxboot/nxboot_main.c
+++ boot/nxboot/nxboot_main.c
@@ -28,28 +28,233 @@
 
 #include <stdio.h>
 #include <syslog.h>
+#include <nuttx/ascii.h>
+#include <sys/param.h>
 
 #include <nxboot.h>
 #include <sys/boardctl.h>
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+typedef struct progress_msgs_s
+{
+  enum progress_msg_e idx; /* Index to the message */
+  const char         *msg; /* Corresponsing text message */
+} progress_msgs_t;
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+bool progress_dots_started = false;
+#ifdef CONFIG_NXBOOT_PRINTF_PROGRESS_PERCENT
+bool progress_percent_started = false;
+#endif
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+#ifdef CONFIG_NXBOOT_PRINTF_PROGRESS
+
+#  ifdef CONFIG_NXBOOT_PRINTF_PROGRESS_PERCENT  
+static const char backtab[] =
+{
+  ASCII_BS, ASCII_BS, ASCII_BS, ASCII_BS, '\0',
+};
+#  endif
+
+static const progress_msgs_t progress_msgs[] =
+  {
+    {startup_msg,              "*** nxboot ***"},
+    {power_reset,              "Power Reset detected, check images only"},
+    {soft_reset,               "Soft reset detected, check for update"},
+    {found_bootable_image,     "Found bootable image, boot from primary"},
+    {no_bootable_image,        "No bootable image found"},
+    {boardioc_image_boot_fail, "Board failed to boot bootable image"},
+    {ramcopy_started,          "Copying bootable image to RAM"},
+    {recovery_revert,          "Reverting image to recovery"},
+    {recovery_create,          "Creating recovery image"},
+    {update_from_update,       "Updating from update image"},
+    {validate_primary,         "Validating primary image"},
+    {validate_recovery,        "Validating recovery image"},
+    {validate_update,          "Validating update image"},
+    {recovery_created,         "Recovery image created"},
+    {recovery_invalid,         "Recovery image invalid, update stopped"},
+    {update_failed,            "Update failed"},
+    {(-1), NULL},
+  };
+#endif /* CONFIG_NXBOOT_PRINTF_PROGRESS */
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+const char *get_msg(enum progress_msg_e idx, const progress_msgs_t *txt);
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+const char *get_msg(enum progress_msg_e idx, const progress_msgs_t *msg)
+{
+  while (msg->idx != idx && msg->msg != NULL)
+    {
+      msg++;
+    }
+
+  return msg->msg;
+}
 
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
 
 /****************************************************************************
+ * Name: nxboot_progress
+ *
+ * Description:
+ *   If enabled, this function prints progress messages to stdout.
+ *   Messages are handled via integer enums, allowing this function to be
+ *   easily replaced if required with no changes needed to the underlying
+ *   code
+ *
+ * Input Parameters:
+ *   type - the progress message type to be printed, as per progress_type_e:
+ *          - nxboot_info:             Prefixes arg. string with "INFO:"
+ *          - nxboot_error:            Prefixes arg. string with "ERR:"
+ *          - nxboot_progress_start:   Prints arg. string with no newline
+ *                                     to allow a ..... sequence
+ *                                     or % remaining to follow
+ *          - nxboot_progress_dot:     Prints a "." to the ..... sequence
+ *          - nxboot_progress_percent: Displays progress as % remaining
+ *          - nxboot_progress_end,     Flags end of a progrees sequence
+ *
+ *   ... - variadic argument:
+ *          - the enum (int) reference to the message string or
+ *          - the % remaining in the case of type: nxboot_progress_percent
+ *
+ * Returned Value:
+ *   None
+ *
+ ****************************************************************************/
+
+void nxboot_progress(enum progress_type_e type, ...)
+{
+#ifdef CONFIG_NXBOOT_PRINTF_PROGRESS
+  va_list arg;
+
+  va_start(arg, type);
+
+  switch (type)
+    {
+      case nxboot_info:
+        {
+          int idx = va_arg(arg, int);
+          dprintf(STDOUT_FILENO, "%s\n", get_msg(idx, progress_msgs));
+        }
+      break;
+      case nxboot_error:
+        {
+          int idx = va_arg(arg, int);
+          dprintf(STDOUT_FILENO, "ERROR: %s\n", get_msg(idx, progress_msgs));
+        }
+      break;
+      case nxboot_progress_start:
+        {
+          int idx = va_arg(arg, int);
+          dprintf(STDOUT_FILENO, "%s", get_msg(idx, progress_msgs));
+          progress_dots_started = true;
+        }
+      break;
+      case nxboot_progress_dot:
+        {
+          assert(progress_dots_started);
+          if (!progress_dots_started)
+            {
+              printf("Progress dot requested "
+                     "but no previous progress start\n");
+            }
+          else
+            {
+              dprintf(STDOUT_FILENO, ".");
+            }
+        }
+      break;
+#ifdef CONFIG_NXBOOT_PRINTF_PROGRESS_PERCENT      
+      case nxboot_progress_percent:
+        {
+          assert(progress_dots_started);
+          if (!progress_dots_started)
+            {
+              printf("Progress percent requested "
+                     "but no previous progress start\n");
+            }
+          else
+            {
+              int percent = va_arg(arg, int);
+              if (!progress_percent_started)
+                {
+                  progress_percent_started = true;
+                  dprintf(STDOUT_FILENO, ": ");
+                }
+              else
+                {
+                  dprintf(STDOUT_FILENO, "%s", backtab);
+                }
+
+              dprintf(STDOUT_FILENO, "%3d%%", MIN(percent, 100));
+            }
+        }
+      break;
+#endif
+      case nxboot_progress_end:
+        {
+          assert(progress_dots_started == false);
+          if (!progress_dots_started)
+            {
+              printf("Progress dot stop requested "
+                     "but no previous progress start\n");
+            }
+          else
+            {
+              dprintf(STDOUT_FILENO, "\n");
+            }
+
+          progress_dots_started = false;
+#ifdef CONFIG_NXBOOT_PRINTF_PROGRESS_PERCENT   
+          progress_percent_started = false;
+#endif
+        }
+      break;
+      default:
+        {
+          assert(false);
+          dprintf(STDOUT_FILENO, "progress: unknown type!\n");
+        }
+      break;
+    }
+
+  va_end(arg);
+#endif /* CONFIG_NXBOOT_PRINTF_PROGRESS */
+}
+
+/****************************************************************************
  * Name: nxboot_main
  *
  * Description:
- *   NuttX bootlaoder entry point.
+ *   NuttX bootloader entry point.
  *
  ****************************************************************************/
 
 int main(int argc, FAR char *argv[])
 {
+  int ret;
   struct boardioc_boot_info_s info;
   bool check_only;
 #ifdef CONFIG_NXBOOT_SWRESET_ONLY
-  int ret;
   FAR struct boardioc_reset_cause_s cause;
 #endif
 
@@ -66,6 +271,7 @@ int main(int argc, FAR char *argv[])
 #endif
 
   syslog(LOG_INFO, "*** nxboot ***\n");
+  nxboot_progress(nxboot_info, startup_msg);
 
 #ifdef CONFIG_NXBOOT_SWRESET_ONLY
   check_only = true;
@@ -77,10 +283,12 @@ int main(int argc, FAR char *argv[])
           cause.cause == BOARDIOC_RESETCAUSE_PIN)
         {
           check_only = false;
+          nxboot_progress(nxboot_info, soft_reset);
         }
       else
         {
           syslog(LOG_INFO, "Power reset detected, performing check only.\n");
+          nxboot_progress(nxboot_info, power_reset);
         }
     }
 #else
@@ -90,15 +298,29 @@ int main(int argc, FAR char *argv[])
   if (nxboot_perform_update(check_only) < 0)
     {
       syslog(LOG_ERR, "Could not find bootable image.\n");
-      return 0;
+      nxboot_progress(nxboot_error, no_bootable_image);
+      return OK;
     }
 
   syslog(LOG_INFO, "Found bootable image, boot from primary.\n");
+  nxboot_progress(nxboot_info, found_bootable_image);
+
+#ifdef CONFIG_NXBOOT_COPY_TO_RAM
+  syslog(LOG_INFO, "Copying image to RAM.\n");
+  nxboot_ramcopy();
+#endif
 
   /* Call board specific image boot */
 
   info.path        = CONFIG_NXBOOT_PRIMARY_SLOT_PATH;
   info.header_size = CONFIG_NXBOOT_HEADER_SIZE;
 
-  return boardctl(BOARDIOC_BOOT_IMAGE, (uintptr_t)&info);
+  ret = boardctl(BOARDIOC_BOOT_IMAGE, (uintptr_t)&info);
+
+  /* Only get here if the board boot fails */
+
+  nxboot_progress(nxboot_error, boardioc_image_boot_fail);
+
+  return ret;
 }
+
