7490640ac4bb80124859077900114675041105d2
hongfengchen <hongfengchen@xiaomi.com>
2025-06-03 12:07:41 +0800

    testing: add unistd.h and pthread.h headers
    Add unistd.h and pthread.h for memorystress_main.c, unistd.h for
    dhm.c and cachetest_main.c, and pthread.h for kv_test_019.c to fix
    missing declarations.
    
    Signed-off-by: hongfengchen <hongfengchen@xiaomi.com>

--- examples/mdnsd/mdnsd_daemon.c
+++ examples/mdnsd/mdnsd_daemon.c
@@ -25,7 +25,6 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-
 #include <stdint.h>
 #include <stdlib.h>
 
--- testing/mm/cachetest/cachetest_main.c
+++ testing/mm/cachetest/cachetest_main.c
@@ -28,6 +28,7 @@
 #include <nuttx/cache.h>
 
 #include <stdio.h>
+#include <unistd.h>
 #include <syslog.h>
 
 /****************************************************************************
