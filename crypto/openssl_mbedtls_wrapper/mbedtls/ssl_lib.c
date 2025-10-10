0a615611b4f3d900b831533f3e22e8c2b7c72607
makejian <makejian@xiaomi.com>
2025-10-10 15:08:23 +0800

    crypto/openssl-wrapper: align SSL_CTX_new declaration with OpenSSL
    Remove the extra rngctx parameter from SSL_CTX_new() to match
    the standard OpenSSL API signature:
      SSL_CTX *SSL_CTX_new(const SSL_METHOD *method)
    
    This improves compatibility with code written for OpenSSL.
    
    Signed-off-by: makejian <makejian@xiaomi.com>

--- crypto/openssl_mbedtls_wrapper/mbedtls/ssl_lib.c
+++ crypto/openssl_mbedtls_wrapper/mbedtls/ssl_lib.c
@@ -260,7 +260,7 @@ int SSL_get_error(const SSL *ssl, int ret_code)
   return ret;
 }
 
-SSL_CTX *SSL_CTX_new(const SSL_METHOD *method, ...)
+SSL_CTX *SSL_CTX_new(const SSL_METHOD *method)
 {
   SSL_CTX *ctx;
   CERT *cert;
