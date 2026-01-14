4faf798c8975c4c72a198e8aca8cb3e3de4e0df3
Matteo Golin <matteo.golin@gmail.com>
2026-01-13 22:45:18 -0500

    sensors/netsensor_stream: Add streaming application for netsensor
    Added a program which allows the user to stream uORB data from their
    selected topic over UDP in a form which can be interpreted by a
    netsensor device.
    
    Signed-off-by: Matteo Golin <matteo.golin@gmail.com>

---
+++ sensors/netsensor_stream/helptext.h
@@ -0,0 +1,6 @@
+#define HELP_TEXT \
+"USAGE:\n    netsensor_stream [options] topic\n\nARGUMENTS:\n    topic       " \
+"   The uORB topic to stream (including devno). Ex: sensor_baro0\n\nOPTIONS:" \
+"    -h             Display this help message and quit.\n    -a <devno>     S" \
+"pecify the IP address to stream data to. Default: 127.0.0.1\n    -p <port>  " \
+"    Specify the UDP port to stream data to. Default: 5555\n"
