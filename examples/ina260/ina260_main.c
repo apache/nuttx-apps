ec27ef4e3077f54320fca6a9c9a1489c41048fe5
Jean THOMAS <jean@lambdaconcept.com>
2026-02-24 12:13:15 +0100

    examples: add ina260
    Add example application for INA260 current/voltage/power sensor driver.
    
    Signed-off-by: Jean THOMAS <jean@lambdaconcept.com>

---
+++ examples/ina260/ina260_main.c
@@ -0,0 +1,84 @@
+/****************************************************************************
+ * apps/examples/ina260/ina260_main.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <nuttx/config.h>
+#include <nuttx/sensors/ina260.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#ifndef CONFIG_INA260_DEVICE_PATH
+#  define CONFIG_INA260_DEVICE_PATH "/dev/ina260"
+#endif
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * ina260_main
+ ****************************************************************************/
+
+int main(int argc, FAR char *argv[])
+{
+  struct ina260_s sample;
+  int fd;
+  int ret;
+
+  fd = open(CONFIG_INA260_DEVICE_PATH, O_RDWR);
+  if (fd < 0)
+    {
+      int errcode = errno;
+      printf("ERROR: Failed to open %s: %d\n",
+             CONFIG_INA260_DEVICE_PATH, errcode);
+      return EXIT_FAILURE;
+    }
+
+  while (1)
+    {
+      ret = read(fd, &sample, sizeof(sample));
+      if (ret != sizeof(sample))
+        {
+          break;
+        }
+
+      printf("U=%6" PRIu32 "mV I=%6" PRIi32 "mA P=%6" PRIu32 "mW\n",
+             sample.voltage, sample.current, sample.power);
+
+      usleep(500000);
+    }
+
+  close(fd);
+  return 0;
+}
