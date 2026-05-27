2b5899c7d4b277c1977e6e14d8c0ef8a9921782e
Matteo Golin <matteo.golin@gmail.com>
2026-05-27 19:32:57 -0400

    apps/nxinit: Make init.rc file path configurable
    The init.rc file path is now configurable to allow users to choose where
    to put the startup script. This is useful for devices that mount
    external media to a special directory like `/sd`.
    
    Signed-off-by: Matteo Golin <matteo.golin@gmail.com>

--- system/nxinit/init.c
+++ system/nxinit/init.c
@@ -180,7 +180,7 @@ int main(int argc, FAR char *argv[])
         }
     }
 
-  r = init_parse_config_file(parser, "/etc/init.d/init.rc");
+  r = init_parse_config_file(parser, CONFIG_SYSTEM_NXINIT_RC_FILE_PATH);
   if (r < 0)
     {
       goto out;
