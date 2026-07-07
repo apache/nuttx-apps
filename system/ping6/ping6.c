707b24e5cbdc3b934c848d3decdb49f41b6cd4f1
Alan Carvalho de Assis <acassis@gmail.com>
2026-07-07 19:13:22 -0300

    system/ping: Fix a segmentation fault when using ping
    nsh> ping google.com
    [   37.180000] dns_query_error: ERROR: IPv4 dns_recv_response fa: -84,
                   server address: 8.8.8.8
    Segmentation fault.
    
    Signed-off-by: Alan C. Assis <acassis@gmail.com>

--- system/ping6/ping6.c
+++ system/ping6/ping6.c
@@ -286,6 +286,8 @@ int main(int argc, FAR char *argv[])
   int exitcode;
   int option;
 
+  memset(&info, 0, sizeof(info));
+
   info.count     = ICMPv6_NPINGS;
   info.datalen   = ICMPv6_PING6_DATALEN;
   info.delay     = ICMPv6_POLL_DELAY;
