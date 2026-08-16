0eb44a108597dbf8731770e9be7a33ddc08537be
aviralgarg05 <gargaviral99@gmail.com>
2026-08-16 12:54:12 +0530

    system/nxstore: add an LVGL app-store front end
    Add an LVGL front end for browsing, installing, updating, and launching nxpkg entries. Keep network and install work off the UI path, and stop launched apps with SIGTERM.
    
    Signed-off-by: aviralgarg05 <gargaviral99@gmail.com>

--- system/nxpkg/pkg.h
+++ system/nxpkg/pkg.h
@@ -29,9 +29,9 @@
 
 #include <nuttx/config.h>
 
-#include <limits.h>
+#include <system/nxpkg.h>
+
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -39,7 +39,6 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
-#define PKG_ROOT_DIR          CONFIG_SYSTEM_NXPKG_ROOT
 #define PKG_REPO_DIR          PKG_ROOT_DIR
 #define PKG_REPO_INDEX        PKG_ROOT_DIR "/index.jsn"
 #define PKG_REPO_SOURCE       PKG_ROOT_DIR "/repo.url"
@@ -47,21 +46,6 @@
 #define PKG_STORE_DIR         PKG_ROOT_DIR "/pkgs"
 #define PKG_TMP_DIR           PKG_ROOT_DIR "/tmp"
 #define PKG_TMP_PKG_DIR       PKG_ROOT_DIR "/tmp/pkg"
-
-#define PKG_NAME_MAX          63
-#define PKG_VERSION_MAX       31
-#define PKG_ARCH_MAX          31
-#define PKG_COMPAT_MAX        63
-#define PKG_DESCRIPTION_MAX   127
-#define PKG_CATEGORY_MAX      31
-#define PKG_HASH_HEX_LEN      64
-/* Keep repository metadata memory bounded. */
-
-#define PKG_INDEX_MAX         16
-#define PKG_INSTALLED_MAX     16
-#define PKG_INSTALLED_VERSIONS_MAX 8
-#define PKG_LAUNCH_ARGS_MAX   8
-#define PKG_LAUNCH_ARG_MAX    127
 
 /* Bound metadata allocations and artifact downloads. */
 
@@ -92,12 +76,6 @@ static inline void pkg_free(void *ptr)
  * Public Types
  ****************************************************************************/
 
-enum pkg_payload_type_e
-{
-  PKG_PAYLOAD_ELF = 0,
-  PKG_PAYLOAD_SHARED_LIB
-};
-
 enum pkg_txn_state_e
 {
   PKG_TXN_IDLE = 0,
@@ -109,46 +87,6 @@ enum pkg_txn_state_e
   PKG_TXN_CLEANUP,
   PKG_TXN_FAILED,
   PKG_TXN_RESTORE
-};
-
-struct pkg_manifest_s
-{
-  char name[PKG_NAME_MAX + 1];
-  char version[PKG_VERSION_MAX + 1];
-  char arch[PKG_ARCH_MAX + 1];
-  char compat[PKG_COMPAT_MAX + 1];
-  char artifact[PATH_MAX];
-  char sha256[PKG_HASH_HEX_LEN + 1];
-  char launch_args[PKG_LAUNCH_ARGS_MAX][PKG_LAUNCH_ARG_MAX + 1];
-  char description[PKG_DESCRIPTION_MAX + 1];
-  char category[PKG_CATEGORY_MAX + 1];
-  char icon[PATH_MAX];
-  enum pkg_payload_type_e type;
-  size_t launch_argc;
-};
-
-struct pkg_index_s
-{
-  struct pkg_manifest_s manifests[PKG_INDEX_MAX];
-  size_t count;
-};
-
-struct pkg_installed_entry_s
-{
-  char name[PKG_NAME_MAX + 1];
-  char current[PKG_VERSION_MAX + 1];
-  char previous[PKG_VERSION_MAX + 1];
-  char arch[PKG_ARCH_MAX + 1];
-  char compat[PKG_COMPAT_MAX + 1];
-  char versions[PKG_INSTALLED_VERSIONS_MAX][PKG_VERSION_MAX + 1];
-  enum pkg_payload_type_e type;
-  size_t version_count;
-};
-
-struct pkg_installed_db_s
-{
-  struct pkg_installed_entry_s entries[PKG_INSTALLED_MAX];
-  size_t count;
 };
 
 /****************************************************************************
@@ -184,13 +122,6 @@ int pkg_store_format_lock_path(FAR char *buffer, size_t size,
 int pkg_store_format_download_path(FAR char *buffer, size_t size,
                                    FAR const char *name,
                                    FAR const char *version);
-int pkg_store_format_payload_path(FAR char *buffer, size_t size,
-                                  FAR const char *name,
-                                  FAR const char *version,
-                                  FAR const char *artifact);
-int pkg_store_format_manifest_path(FAR char *buffer, size_t size,
-                                   FAR const char *name,
-                                   FAR const char *version);
 int pkg_store_read_text(FAR const char *path, FAR char **buffer);
 int pkg_store_write_text_atomic(FAR const char *path, FAR const char *text);
 int pkg_store_copy_file(FAR const char *src, FAR const char *dest);
@@ -207,17 +138,7 @@ int pkg_hash_file_sha256(FAR const char *path,
 
 int pkg_metadata_load_index_path(FAR const char *path,
                                  FAR struct pkg_index_s *index);
-int pkg_metadata_load_index(FAR struct pkg_index_s *index);
-int pkg_metadata_load_manifest_path(FAR const char *path,
-                                    FAR struct pkg_manifest_s *manifest);
-FAR const struct pkg_manifest_s *
-pkg_metadata_find_latest(FAR const struct pkg_index_s *index,
-                         FAR const char *name);
-int pkg_metadata_load_installed(FAR struct pkg_installed_db_s *db);
 int pkg_metadata_save_installed(FAR const struct pkg_installed_db_s *db);
-FAR struct pkg_installed_entry_s *
-pkg_metadata_find_installed(FAR struct pkg_installed_db_s *db,
-                            FAR const char *name);
 int pkg_metadata_write_manifest(FAR const char *path,
                                 FAR const struct pkg_manifest_s *manifest);
 int pkg_metadata_print_installed(FAR FILE *stream,
@@ -230,15 +151,9 @@ int pkg_txn_clear_state(FAR const char *name);
 bool pkg_source_is_url(FAR const char *source);
 int pkg_resolve_artifact_source(FAR char *buffer, size_t size,
                                 FAR const struct pkg_manifest_s *manifest);
-int pkg_resolve_icon_source(FAR char *buffer, size_t size,
-                            FAR const struct pkg_manifest_s *manifest);
-int pkg_acquire_source(FAR const char *source, FAR const char *dest);
 int pkg_lock_create(FAR const char *path);
 void pkg_lock_remove(FAR const char *path);
 void pkg_reclaim_stale_lock(FAR const char *path);
-int pkg_sync(FAR const char *source);
-int pkg_install(FAR const char *name);
-int pkg_uninstall(FAR const char *name);
 int pkg_rollback(FAR const char *name);
 int pkg_available(FAR FILE *stream);
 int pkg_list(FAR FILE *stream);
