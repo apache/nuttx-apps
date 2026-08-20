bff59ce6650c86a4db609f2fdd711e2a6e66a1c8
Bogdan <Bogdan4ik0759@gmail.com>
2026-08-20 17:08:54 +0300

    testing/nettest: add cmocka test for local socket SCM_RIGHTS
    Add CMocka regression test case to verify local socket SCM_RIGHTS
    behavior when the file descriptor table is exhausted.
    
    Signed-off-by: Bogdan <Bogdan4ik0759@gmail.com>

---
+++ testing/nettest/local/test_local.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+ * apps/testing/nettest/local/test_local.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __APPS_TESTING_NETTEST_LOCAL_TEST_LOCAL_H
+#define __APPS_TESTING_NETTEST_LOCAL_TEST_LOCAL_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/compiler.h>
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: test_local_group_setup
+ ****************************************************************************/
+
+int test_local_group_setup(FAR void **state);
+
+/****************************************************************************
+ * Name: test_local_group_teardown
+ ****************************************************************************/
+
+int test_local_group_teardown(FAR void **state);
+
+/****************************************************************************
+ * Name: test_local_scm_rights
+ ****************************************************************************/
+
+void test_local_scm_rights(FAR void **state);
+
+#endif /* __APPS_TESTING_NETTEST_LOCAL_TEST_LOCAL_H */
