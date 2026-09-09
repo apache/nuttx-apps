24f42ef27c918eab6d5dfce1d9824644e86cebfa
likun17 <likun17@xiaomi.com>
2026-09-09 19:58:03 +0800

    system/uorb: add voltage, current and power topics
    Declare and define the uORB metadata for the voltage, current and power
    types, and register them in g_sensor_list[] so that orb_get_meta() and
    uorb_listener can resolve them by name.
    
    Signed-off-by: likun17 <likun17@xiaomi.com>

---
+++ system/uorb/sensor/voltage.c
@@ -0,0 +1,41 @@
+/****************************************************************************
+ * apps/system/uorb/sensor/voltage.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <sensor/voltage.h>
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+#ifdef CONFIG_DEBUG_UORB
+static const char sensor_voltage_format[] =
+  "timestamp:%" PRIu64 ",voltage:%hf";
+#endif
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+ORB_DEFINE(sensor_voltage, struct sensor_voltage,
+           sensor_voltage_format);
