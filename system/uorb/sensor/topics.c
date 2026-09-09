5632755acff3a4f08e52bcf355b45fc7436562b7
likun17 <likun17@xiaomi.com>
2026-09-09 19:58:30 +0800

    system/uorb: add resistance, conductivity, energy and charge topics
    Declare and define the uORB metadata for the resistance, conductivity,
    energy and charge types, and register them in g_sensor_list[] so that
    orb_get_meta() and uorb_listener can resolve them by name.
    
    Signed-off-by: likun17 <likun17@xiaomi.com>

--- system/uorb/sensor/topics.c
+++ system/uorb/sensor/topics.c
@@ -35,10 +35,13 @@
 #include <sensor/angle.h>
 #include <sensor/baro.h>
 #include <sensor/cap.h>
+#include <sensor/charge.h>
 #include <sensor/co2.h>
+#include <sensor/conductivity.h>
 #include <sensor/current.h>
 #include <sensor/dust.h>
 #include <sensor/ecg.h>
+#include <sensor/energy.h>
 #include <sensor/eng.h>
 #include <sensor/force.h>
 #include <sensor/gas.h>
@@ -66,6 +69,7 @@
 #include <sensor/ppgd.h>
 #include <sensor/ppgq.h>
 #include <sensor/prox.h>
+#include <sensor/resistance.h>
 #include <sensor/rgb.h>
 #include <sensor/rotation.h>
 #include <sensor/step_counter.h>
@@ -89,11 +93,14 @@ static FAR const struct orb_metadata *g_sensor_list[] =
   ORB_ID(sensor_hinge_angle),
   ORB_ID(sensor_baro),
   ORB_ID(sensor_cap),
+  ORB_ID(sensor_charge),
   ORB_ID(sensor_co2),
+  ORB_ID(sensor_conductivity),
   ORB_ID(sensor_current),
   ORB_ID(sensor_device_orientation),
   ORB_ID(sensor_dust),
   ORB_ID(sensor_ecg),
+  ORB_ID(sensor_energy),
   ORB_ID(sensor_eng),
   ORB_ID(sensor_force),
   ORB_ID(sensor_gas),
@@ -136,6 +143,7 @@ static FAR const struct orb_metadata *g_sensor_list[] =
   ORB_ID(sensor_ppgd),
   ORB_ID(sensor_ppgq),
   ORB_ID(sensor_prox),
+  ORB_ID(sensor_resistance),
   ORB_ID(sensor_rgb),
   ORB_ID(sensor_rotation),
   ORB_ID(sensor_significant_motion),
@@ -173,6 +181,7 @@ FAR const struct orb_metadata *orb_get_meta(FAR const char *name)
   for (i = 0; g_sensor_list[i]; i++)
     {
       size_t len = strlen(g_sensor_list[i]->o_name);
+
       if ((!strncmp(g_sensor_list[i]->o_name, name, len))
           && (name[len] == '\0' || isdigit(name[len])))
         {
